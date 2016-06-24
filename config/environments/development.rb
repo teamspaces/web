61507afd6d416039a78b3216bb65cd0742694539
Martin Samami <martin@digitalkookie.io>
2016-06-24 12:05:46 +0200

    Include devise test helpers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Enable web console for docker environments
+  config.web_console.whitelisted_ips = '192.168.99.1'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActiveSupport::IntegrationTest
+  include Devise::TestHelpers
 end
