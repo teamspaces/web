4c24aa73e3406b8969d69f6d0e0357c5b6e5ee4d
Martin Samami <martin@digitalkookie.io>
2016-10-04 20:29:11 +0200

    Fix syntax from old to new testing syntax

--- test/controllers/spaces_controller_test.rb
+++ test/controllers/spaces_controller_test.rb
@@ -1,48 +1,4 @@
 require 'test_helper'
 
-class SpacesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @space = spaces(:furrow)
-  end
-
-  test "should get index" do
-    get spaces_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_space_url
-    assert_response :success
-  end
-
-  test "should create space" do
-    assert_difference('Space.count') do
-      post spaces_url, params: { space: { name: @space.name } }
-    end
-
-    assert_redirected_to space_path(Space.last)
-  end
-
-  test "should show space" do
-    get space_url(@space)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_space_url(@space)
-    assert_response :success
-  end
-
-  test "should update space" do
-    patch space_url(@space), params: { space: { name: @space.name } }
-    assert_redirected_to space_path(@space)
-  end
-
-  test "should destroy space" do
-    assert_difference('Space.count', -1) do
-      delete space_url(@space)
-    end
-
-    assert_redirected_to spaces_path
-  end
+describe SpacesController do
 end
