b520e1b6896a6b1fd4272dcbdc86bf314867c75d
Martin Samami <martin@digitalkookie.io>
2016-10-18 02:25:34 +0330

    Edit works

--- app/models/collab_page.rb
+++ app/models/collab_page.rb
@@ -1,11 +1,10 @@
 class CollabPage
   include Mongoid::Document
-  store_in collection: "collab_pages"
 
   field :_id, type: String
   field :ops, type: Array
   field :_type, type: String
   field :_v, type: Integer
   field :_m, type: Hash
-  field :_0, type: BSON::ObjectId
+  field :_o, type: BSON::ObjectId
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,30 @@
 class Page < ApplicationRecord
   belongs_to :space
   validates :space, presence: true
+
+  # TODO: Temporary approach to creating documents for collaboration. Move this
+  # into a service object or anything prettier than this.
+  after_create :create_collab_page
+  after_destroy :destroy_collab_page
+
+  def create_collab_page
+    collab_page
+  end
+
+  def collab_page
+    CollabPage.where(id: "#{id}").first_or_create!({
+        _type: "http://sharejs.org/types/rich-text/v1",
+        _m: {
+          ctime: Time.now.to_i.to_f,
+          mtime: Time.now.to_i.to_f
+        },
+        _v: 1,
+        ops: [],
+        _o: BSON::ObjectId.new
+      })
+  end
+
+  def destroy_collab_page
+    collab_page.destroy
+  end
 end
