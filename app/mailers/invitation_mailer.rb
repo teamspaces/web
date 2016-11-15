7806c98b9ca76b58e25bdbe4ab2637cb3e792536
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-11-15 12:35:01 +0100

    test redirect url in invitation mails

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,6 +3,7 @@ class InvitationMailer < ApplicationMailer
 
   def join_team(invitation)
     @invitation = invitation
+    @invitation_url = landing_url(invitation_token: @invitation.token)
     mail(to: @invitation.email, subject: "Join #{invitation.team} in Spaces")
   end
 end
