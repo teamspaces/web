c1d7b308b5e6f471e7bed44b1360a1959e761c05
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-11-15 22:18:44 +0100

    test service objects

--- test/services/invitation/create_and_send_join_team_invite_mail_test.rb
+++ test/services/invitation/create_and_send_join_team_invite_mail_test.rb
@@ -5,28 +5,24 @@ describe Invitation::CreateAndSendJoinTeamInviteMail, :model do
   let(:team) { teams(:furrow) }
   let(:service) { Invitation::CreateAndSendJoinTeamInviteMail }
 
-  def team_params(email)
-    { invitation: { email: email} }
-  end
-
   it "creates invitation" do
-    success, invitation = service.call(team_params("ken@o.es"), team, user)
+    success, invitation = service.call({ email: "ken@o.es" }, team, user)
 
     assert success
     assert_equal user, invitation.user
     assert_equal team, invitation.team
   end
 
-  it "sends join team email invitation" do
+  it "sends join team mail-invitation" do
     InvitationMailer.expects(:join_team).returns(email_mock = mock)
     email_mock.expects(:deliver_later)
 
-    service.call(team_params("kay@o.es"), team, user)
+    service.call({ email: "kay@o.es" }, team, user)
   end
 
   context "invalid params" do
     it "does not create invitation" do
-      success, invitation = service.call(team_params("invalid"), team, user)
+      success, invitation = service.call({ email: "invalid" }, team, user)
 
       refute success
       assert invitation.errors[:email]
--- test/services/team/create_team_for_user_test.rb
+++ test/services/team/create_team_for_user_test.rb
@@ -3,23 +3,19 @@ require "test_helper"
 describe Team::CreateTeamForUser, :model do
   let(:user) { users(:lars) }
 
-  def team_params(name)
-    { team: { name: name } }
-  end
-
   it "creates team" do
     assert_difference -> { Team.count }, 1 do
       name = "alpa"
-      success, team = Team::CreateTeamForUser.call(team_params(name), user)
+      success, team = Team::CreateTeamForUser.call({ name: name }, user)
 
       assert success
       assert name, team.name
     end
   end
 
-  it "creates team_member as primary owner" do
+  it "creates team_member as primary owner associated to user" do
     assert_difference -> { TeamMember.count }, 1 do
-      success, team = Team::CreateTeamForUser.call(team_params("bar"), user)
+      success, team = Team::CreateTeamForUser.call({ name: "bar" }, user)
 
       assert_equal user, team.members.first.user
       assert_equal TeamMember::Roles::PRIMARY_OWNER, team.members.first.role
