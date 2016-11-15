0f9ba78f44018be21e9a3228f7a3cd06d992bbba
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-11-15 14:37:18 +0100

    remove host settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => "test.yourhost.com" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -54,6 +53,7 @@ Rails.application.configure do
   # https://github.com/rails/rails/issues/24990#issuecomment-223614015
   #
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
   # Enable web console for docker environments
   config.web_console.whitelisted_ips = ['192.168.99.1', '172.18.0.1']
 end
