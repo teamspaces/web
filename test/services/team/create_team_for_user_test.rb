932a04185122fbc879a3a1faa0826c721e9e368d
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-11-15 21:46:29 +0100

    test service object

--- test/services/team/create_team_for_user_test.rb
+++ test/services/team/create_team_for_user_test.rb
@@ -1,12 +1,28 @@
 require "test_helper"
 
 describe Team::CreateTeamForUser, :model do
+  let(:user) { users(:lars) }
+
+  def team_params(name)
+    { team: { name: name } }
+  end
 
   it "creates team" do
-    refute true
+    assert_difference -> { Team.count }, 1 do
+      name = "alpa"
+      success, team = Team::CreateTeamForUser.call(team_params(name), user)
+
+      assert success
+      assert name, team.name
+    end
   end
 
   it "creates team_member as primary owner" do
-    refute true
+    assert_difference -> { TeamMember.count }, 1 do
+      success, team = Team::CreateTeamForUser.call(team_params("bar"), user)
+
+      assert_equal user, team.members.first.user
+      assert_equal TeamMember::Roles::PRIMARY_OWNER, team.members.first.role
+    end
   end
 end
