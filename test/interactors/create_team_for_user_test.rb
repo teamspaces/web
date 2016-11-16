38912a7aea627fa0c2f57b82e2e9c36c115715d7
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-11-16 16:23:15 +0100

    rename

--- test/interactors/create_team_for_user_test.rb
+++ test/interactors/create_team_for_user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe CreateTeamForUser, :model do
   subject { CreateTeamForUser }
   let(:user) { users(:lars) }
-  let(:pirmary_role) { TeamMember::Roles::PRIMARY_OWNER }
+  let(:primary_role) { TeamMember::Roles::PRIMARY_OWNER }
 
   it "creates team" do
     assert_difference -> { Team.count }, 1 do
@@ -20,7 +20,7 @@ describe CreateTeamForUser, :model do
       result = subject.call(team_params: { name: "bar" }, user: user)
 
       assert_equal user, result.team.members.first.user
-      assert_equal pirmary_role, result.team.members.first.role
+      assert_equal primary_role, result.team.members.first.role
     end
   end
 end
