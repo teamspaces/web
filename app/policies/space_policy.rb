f8a90608f7cc4096671d15debd1e085ebfa5fbf0
Martin Samami ッ <martin@digitalkookie.io>
2016-11-19 00:17:56 +0100

    Fix context leak

--- app/policies/space_policy.rb
+++ app/policies/space_policy.rb
@@ -4,13 +4,13 @@ class SpacePolicy
   attr_reader :default_context, :space
 
   def initialize(default_context, space)
-    @default_context = default_context
+    @team = default_context.team
     @space = space
   end
 
   def team_space?
-    default_context.team == space.team
+    team == space.team
   end
-
+  
   alias_methods :team_space?, [:show?, :new?, :edit?, :create?, :update?, :destroy?]
 end
