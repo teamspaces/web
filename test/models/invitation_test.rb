644087af9c1b70afbbee176d9dd07e750a0b9da3
EmmanuelSTs <stadleremmanuel1@gmail.com>
2016-11-21 10:02:32 +0100

    Fix/active record associations (#49)

--- test/models/invitation_test.rb
+++ test/models/invitation_test.rb
@@ -4,8 +4,17 @@ describe Invitation do
   let(:team) { teams(:furrow) }
   let(:user)  { users(:lars) }
 
-  it "generates token before creation" do
-    invitation = team.invitations.create(user: user, email: "n@web.com")
-    assert invitation.token
+  should belong_to(:team)
+  should belong_to(:user)
+  should have_one(:invitee).with_foreign_key("invitee_user_id")
+                           .class_name("User")
+
+  should validate_uniqueness_of(:token)
+
+  describe "#create" do
+    it "generates token" do
+      invitation = team.invitations.create(user: user, email: "n@web.com")
+      assert invitation.token
+    end
   end
 end
