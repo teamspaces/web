13c0cf74312fd217c25ad4fc2c22dd453cbfbb8d
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-11-21 11:43:28 +0100

    improve

--- app/controllers/subdomain_base_controller.rb
+++ app/controllers/subdomain_base_controller.rb
@@ -2,7 +2,7 @@ class SubdomainBaseController < ApplicationController
   before_action :check_team_membership
 
   def current_team
-    Team.find_by_subdomain(request.subdomain)
+    Team.find_by(subdomain: request.subdomain)
   end
 
   private
--- test/policies/team_policy/scope_test.rb
+++ test/policies/team_policy/scope_test.rb
@@ -3,12 +3,11 @@ require "test_helper"
 describe TeamPolicy::Scope, :model do
   let(:user) { users(:lars) }
   let(:team) { teams(:furrow) }
-  let(:team_space) { spaces(:furrow) }
-  let(:default_context) { DefaultPolicy::Context.new(user, team) }
+  let(:default_context) { DefaultContext.new(user, team) }
 
   describe "#resolve" do
     it "returns user's teams" do
-      policy_filtered_teams  = TeamPolicy::Scope.new(default_context, team).resolve
+      policy_filtered_teams  = TeamPolicy::Scope.new(default_context, Team).resolve
 
       assert_equal user.teams, policy_filtered_teams
     end
