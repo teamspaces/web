0d5b698299739dd2a468f5408c5c4f477f9c9f59
EmmanuelSTs <stadleremmanuel1@gmail.com>
2016-11-21 14:09:50 +0100

    Add subdomain field to team model (#47)
    * Add subdomain field to team model
    * Add subdomain validator

--- test/forms/create_team_for_user_form_test.rb
+++ test/forms/create_team_for_user_form_test.rb
@@ -1,19 +1,43 @@
 require "test_helper"
 
 describe CreateTeamForUserForm, :model do
+  let(:team_name) { "nasa" }
+  let(:team_subdomain) { "worldwide"}
   let(:user) { users(:lars) }
   let(:existing_team) { teams(:furrow) }
-  subject { CreateTeamForUserForm.new(name: "team_name", user: user) }
+
+  subject do
+   CreateTeamForUserForm.new(name: team_name, user: user,
+                             subdomain: team_subdomain)
+  end
 
   describe "validations" do
     should validate_presence_of(:user)
     should validate_presence_of(:name)
+    should validate_presence_of(:subdomain)
 
-    it "validates uniqueness of name" do
-      subject.name = existing_team.name
+    it "subdomain uniqueness" do
+      subject.subdomain = existing_team.subdomain
       subject.save
-      assert_includes subject.errors[:name], "name already in use"
+      assert_includes subject.errors[:subdomain], "has already been taken"
     end
+
+    it "subdomain format" do
+      subject.subdomain = "team/nasa"
+      subject.save
+      assert_includes subject.errors[:subdomain], "must be alphanumeric (or hyphen)"
+
+      subject.subdomain = "what"
+      subject.save
+      assert_includes subject.errors[:subdomain], "cannot be a reserved name"
+    end
+  end
+
+  it "creates team" do
+    subject.save
+
+    assert team_name, subject.team.name
+    assert team_subdomain, subject.team.subdomain
   end
 
   it "creates first team member" do
