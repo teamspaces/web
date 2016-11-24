40dfd49b2248104f3033bd7ca6f8bcc5cac54059
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-11-24 14:40:15 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,33 @@
 Rails.application.routes.draw do
 
-  constraints subdomain: 'what' do
-    resources :teams, only: [:index, :new, :create]
-    resources :pages, only: [:show, :edit, :update, :destroy]
-
-    devise_for :users,
-               skip: [:sessions],
-               controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-
-    devise_scope :user do
-      get :login, to: "devise/sessions#new", as: :new_user_session
-      post :login, to: "devise/sessions#create", as: :user_session
-      delete :logout, to: "devise/sessions#destroy", as: :destroy_user_session
-      get :sign_up, to: 'devise/registrations#new', as: :sign_up
+  constraints lambda { |req| req.subdomain != ENV['DEFAULT_SUBDOMAIN']}  do
+    resources :spaces do
+      resources :pages, only: [:index, :new, :create]
     end
 
-    get :landing, to: "landing#index", as: :landing
+    resources :invitations, only: [:index, :create, :destroy]
 
-    root "landing#index"
+    get :edit, to: 'teams#edit', as: :edit_team
+    get '', to: 'teams#show', as: :team
+    patch '', to: 'teams#update'
+    delete '', to: 'teams#destroy'
   end
 
-  resources :spaces do
-    resources :pages, only: [:index, :new, :create]
+  resources :teams, only: [:index, :new, :create]
+  resources :pages, only: [:show, :edit, :update, :destroy]
+
+  devise_for :users,
+             skip: [:sessions],
+             controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get :login, to: "devise/sessions#new", as: :new_user_session
+    post :login, to: "devise/sessions#create", as: :user_session
+    delete :logout, to: "devise/sessions#destroy", as: :destroy_user_session
+    get :sign_up, to: 'devise/registrations#new', as: :sign_up
   end
 
-  resources :invitations, only: [:index, :create, :destroy]
+  get :landing, to: "landing#index", as: :landing
 
-  get :edit, to: 'teams#edit', as: :edit_team
-  get '', to: 'teams#show', as: :team
-  patch '', to: 'teams#update'
-  delete '', to: 'teams#destroy'
+  root "landing#index"
 end
