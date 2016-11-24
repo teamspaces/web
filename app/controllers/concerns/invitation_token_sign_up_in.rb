340d1aef2ccdcd01429f2dc5d6097a7370b451e4
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-11-24 10:05:29 +0100

    yield Accept invitation

--- app/controllers/concerns/invitation_token_sign_up_in.rb
+++ app/controllers/concerns/invitation_token_sign_up_in.rb
@@ -4,30 +4,31 @@ module InvitationTokenSignUpIn
   #ovverrides devise session create and devise registration create
   def create
     if upon_invitation? && !invitation_matches_email?(params[:user][:email])
-      redirect_and_show_email_invitation_inconsistency
+      redirect_and_show_invitation_email_inconsistency
     else
       super do |user|
-        oben extra service draus machen
-        user.persisted?
-        #user.update_attributes(foo: :bar)
+        if upon_invitation? && user.persisted?
+          AcceptInvitation.call(user: user, token: invitation_from_token_param)
+        end
       end
     end
   end
 
-  def upon_invitation?
-    params[:invitation_token].present? && invitation_from_token_param
-  end
+  private
+    def upon_invitation?
+      params[:invitation_token].present? && invitation_from_token_param
+    end
 
-  def invitation_matches_email?(email)
-    email == invitation_from_token_param.email
-  end
+    def invitation_matches_email?(email)
+      email == invitation_from_token_param.email
+    end
 
-  def redirect_and_show_email_invitation_inconsistency
-    flash[:notice] = t(:invitation_does_not_match_provided_email)
-    redirect_back(fallback_location: landing_path)
-  end
+    def redirect_and_show_invitation_email_inconsistency
+      flash[:notice] = t(:invitation_does_not_match_provided_email)
+      redirect_back(fallback_location: landing_path)
+    end
 
-  def invitation_from_token_param
-    Invitation.find_by(token: params[:invitation_token])
-  end
+    def invitation_from_token_param
+      Invitation.find_by(token: params[:invitation_token])
+    end
 end
