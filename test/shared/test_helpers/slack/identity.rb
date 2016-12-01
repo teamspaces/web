718902bce0ab9fb034a68b22a6cc345e24164f04
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-01 22:29:16 +0100

    add josn query

--- test/shared/test_helpers/slack/identity.rb
+++ test/shared/test_helpers/slack/identity.rb
@@ -10,68 +10,47 @@ class TestHelpers
       def unknown_user
         identity = default_identity
 
-        identity[:user].merge!({ "id": "U2ZILGD39",
-                                  "name": "Maria Balvin",
-                                  "email": "maria@balvin.com" })
-
-        identity[:team].merge!({ "id": "T0C8MBADA",
-                                  "name": "Spaces",
-                                  "domain": "teamspaces" })
-
-        ::Slack::Messages::Message.new(identity)
+  #      identity[:user].merge!({ "id": "U2ZILGD39",
+   #                               "name": "Maria Balvin",
+ #                                 "email": "maria@balvin.com" })
+#
+   #     identity[:team].merge!({ "id": "T0C8MBADA",
+  #                                "name": "Spaces",
+ #                                 "domain": "teamspaces" })
+#
+        #::Slack::Messages::Message.new(identity)
       end
 
       def existing_user
         identity = default_identity
 
-        identity[:user].merge!({ "id": "U2ZKLGE49",
-                                  "name": "Milad Marzano",
-                                  "email": "milad@lui.com" })
+       # identity[:user].merge!({ "id": "U2ZKLGE49",
+       #                           "name": "Milad Marzano",
+       #                           "email": "milad@lui.com" })
 
-        identity[:team].merge!({ "id": "T0C7MBADQ",
-                                  "name": "La Industria",
-                                  "domain": "laindustria" })
+      #  identity[:team].merge!({ "id": "T0C7MBADQ",
+        #                          "name": "La Industria",
+       #                           "domain": "laindustria" })
 
-        ::Slack::Messages::Message.new(identity)
+       # ::Slack::Messages::Message.new(identity)
       end
 
       def invalid
-        ::Slack::Messages::Message.new({
-          "ok": false,
-          "error": "missing_scope",
-          "needed": "identity.basic",
-          "provided": "identify,read,post,client,apps,admin" })
+        default_identity
+        #::Slack::Messages::Message.new({
+        #  "ok": false,
+        #  "error": "missing_scope",
+        #  "needed": "identity.basic",
+        #  "provided": "identify,read,post,client,apps,admin" })
       end
 
       private
 
         def default_identity
-          return {
-            "ok": true,
-            "user": { "name": "Maria Balvin",
-                      "id": "U2ZILGD39",
-                      "email": "maria@balvin.com",
-                      "image_24": "https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_24.jpg",
-                      "image_32": "https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_32.jpg",
-                      "image_48": "https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_48.jpg",
-                      "image_72": "https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_72.jpg",
-                      "image_192": "https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_192.jpg",
-                      "image_512": "https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_192.jpg",
-                      "image_1024": "https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_192.jpg"
-                    },
-            "team": { "id": "T0C8MBADA",
-                      "name": "Spaces",
-                      "domain": "teamspaces",
-                      "image_34": "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0013-34.png",
-                      "image_44": "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0013-44.png",
-                      "image_68": "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0013-68.png",
-                      "image_88": "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0013-88.png",
-                      "image_102": "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0013-102.png",
-                      "image_132": "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0013-132.png",
-                      "image_230": "https://a.slack-edge.com/9e9be/img/avatars-teams/ava_0013-230.png",
-                      "image_default": true
-                    }
-          }
+        #  @default_identity || begin
+
+            OmniAuth::AuthHash.new(JSON.parse("{\"ok\":true,\"user\":{\"id\":\"U2ZKLGD39\",\"team_id\":\"T0C8MBADQ\",\"name\":\"emmanuel\",\"deleted\":false,\"status\":null,\"color\":\"3c989f\",\"real_name\":\"Emmanuel Stadler\",\"tz\":\"Europe/Amsterdam\",\"tz_label\":\"Central European Time\",\"tz_offset\":3600,\"profile\":{\"first_name\":\"Emmanuel\",\"last_name\":\"Stadler\",\"avatar_hash\":\"7f9c724553d0\",\"image_24\":\"https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_24.jpg\",\"image_32\":\"https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_32.jpg\",\"image_48\":\"https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_48.jpg\",\"image_72\":\"https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_72.jpg\",\"image_192\":\"https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_192.jpg\",\"image_512\":\"https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_192.jpg\",\"image_1024\":\"https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_192.jpg\",\"image_original\":\"https://avatars.slack-edge.com/2016-11-13/104047724484_7f9c724553d01d5edd8f_original.jpg\",\"title\":\"\",\"phone\":\"\",\"skype\":\"\",\"real_name\":\"Emmanuel Stadler\",\"real_name_normalized\":\"Emmanuel Stadler\",\"email\":\"emmanuel@furrow.io\"},\"is_admin\":true,\"is_owner\":true,\"is_primary_owner\":false,\"is_restricted\":false,\"is_ultra_restricted\":false,\"is_bot\":false,\"has_2fa\":false}}"))
+         # end
         end
     end
   end
