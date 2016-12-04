8ec292f1358987287ac2fe70a272d8a080289f1c
EmmanuelSTs <stadleremmanuel1@gmail.com>
2016-12-04 21:43:46 +0100

    Accept Invitations (Cookie Strategy) (#61)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   include HTTPBasicAuthentication
   include TokenParamLogin
-  include UserAfterSignInPath
+  include User::AfterSignInPath
+  include InvitationCookie
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
--- lib/slack/identity/uid.rb
+++ lib/slack/identity/uid.rb
@@ -3,4 +3,9 @@ class Slack::Identity::UID
   def self.build(slack_identity)
     "#{slack_identity.user.id}-#{slack_identity.user.team_id}"
   end
+
+  def self.parse(uid)
+    user_id, team_id = uid.split("-")
+    { user_id: user_id, team_id: team_id }
+  end
 end
