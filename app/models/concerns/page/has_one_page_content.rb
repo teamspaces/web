0e860c6655426beee98a3eb88e9502eddc58341d
Martin Samami <martin@digitalkookie.io>
2016-12-11 19:59:56 +0100

    Givet this a spin

--- app/models/concerns/page/has_one_page_content.rb
+++ app/models/concerns/page/has_one_page_content.rb
@@ -1,18 +1,20 @@
-module Page::HasOnePageContent
-  extend ActiveSupport::Concern
+class Page
+  module HasOnePageContent
+    extend ActiveSupport::Concern
 
-  included do
-    has_one :page_content
-    after_create :create_page_content
-  end
-
-  def contents
-    page_content.contents
-  end
-
-  private
-
-    def create_page_content
-      PageContent.find_or_create_by(page_id: id)
+    included do
+      has_one :page_content
+      after_create :create_page_content
     end
+
+    def contents
+      page_content.contents
+    end
+
+    private
+
+      def create_page_content
+        PageContent.find_or_create_by(page_id: id)
+      end
+  end
 end
