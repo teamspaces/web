c8da34b629715745b32b90d76c7ddb1b8274d154
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-12 22:38:15 +0100

    test invitation jobs

--- test/jobs/send_email_invitation_job_test.rb
+++ test/jobs/send_email_invitation_job_test.rb
@@ -1,12 +1,13 @@
 require "test_helper"
 
 describe SendEmailInvitationJob, :model do
+  subject { SendEmailInvitationJob }
   let(:invitation) { invitations(:jonas_at_furrow) }
 
-  it "sends team mail-invitation" do
+  it "sends invitation as email" do
     InvitationMailer.expects(:join_team).returns(email_mock = mock)
     email_mock.expects(:deliver_later)
 
-    SendEmailInvitationJob.perform_now(invitation.id)
+    subject.perform_now(invitation.id)
   end
 end
