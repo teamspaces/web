1c0f451b6741339e33274c18bd15f04916ff9cce
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-14 17:52:42 +0100

    leave hacks in /lib-folder

--- lib/alias_methods.rb
+++
@@ -1,5 +0,0 @@
-module AliasMethods
-  def alias_methods(alias_to, methods)
-    methods.each { |method| alias_method method, alias_to }
-  end
-end
--- lib/http_basic_authentication.rb
+++
@@ -1,9 +0,0 @@
-module HTTPBasicAuthentication
-  extend ActiveSupport::Concern
-  included do
-    if ENV["ENABLE_BASIC_AUTH"] == "true"
-      http_basic_authenticate_with name: ENV["BASIC_AUTH_USERNAME"],
-                                   password: ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
-end
