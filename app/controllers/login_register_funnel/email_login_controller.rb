856192dc4034cb63fd65d310ef7bcbaff47da699
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-15 12:44:56 +0100

    email login

--- app/controllers/login_register_funnel/email_login_controller.rb
+++ app/controllers/login_register_funnel/email_login_controller.rb
@@ -2,11 +2,27 @@ class LoginRegisterFunnel::EmailLoginController < LoginRegisterFunnelController
   before_action :check_user_completed_preceding_funnel_steps
 
   def new
-    @user = User.new(email: session[:user_email_address])
+    @email_login_form = LoginRegisterFunnel::EmailLoginForm.new(email: session[:user_email_address])
   end
 
   def create
+    @email_login_form = LoginRegisterFunnel::EmailLoginForm.new(email: session[:user_email_address],
+                                                                password: login_params[:password])
 
+    if @email_login_form.valid?
+      user = @email_login_form.user
+
+      sign_in user
+      redirect_to after_sign_in_path_for user
+    else
+      render :new
+    end
   end
 
+
+  private
+
+    def login_params
+      params.require(:login_register_funnel_email_login_form).permit(:password)
+    end
 end
