f3442a705a4bb1163d30bc60eb120b3def3cb472
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-18 17:17:10 +0100

    invitation cookie over all subdomains

--- app/controllers/concerns/invitation_cookie.rb
+++ app/controllers/concerns/invitation_cookie.rb
@@ -3,7 +3,9 @@ module InvitationCookie
 
   def set_invitation_cookie_from_params
     if params[:invitation_token]
-      cookies.signed[:invitation_token] = { value: params[:invitation_token] }
+      cookies.signed[:invitation_token] = { value: params[:invitation_token],
+                                            domain: :all, tld_length: 2,
+                                            expires: 1.hour.from_now }
     end
   end
 
