9519996407ca24e6c133edcd7fe4b86c35b5ae74
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-20 11:48:30 +0100

    test invitation decorator

--- app/decorators/invitation_decorator.rb
+++ app/decorators/invitation_decorator.rb
@@ -2,18 +2,22 @@ class InvitationDecorator < Draper::Decorator
   delegate_all
 
   def email_invitation?
-    object.email
+    invitee_email_kown? and not slack_invitation?
   end
 
   def slack_invitation?
-    object.slack_user_id
+    object.slack_user_id.present?
   end
 
   def already_accepted?
-    object.invitee_user_id
+    object.invitee_user_id.present?
+  end
+
+  def invitee_email_kown?
+    object.email.present?
   end
 
   def invitee_is_registered_email_user?
-    User.find_by(email: object.email, allow_email_login: true)
+    object.email and User.find_by(email: object.email, allow_email_login: true).present?
   end
 end
