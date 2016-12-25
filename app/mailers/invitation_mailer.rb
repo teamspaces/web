4591284e5a8a607b4a5e6409b03967ad3d8a761d
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-26 00:20:06 +0100

    add missing bracket

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -2,7 +2,7 @@ class InvitationMailer < ApplicationMailer
 
   def join_team(invitation)
     @invitation = invitation
-    @invitation_url = accept_invitation_url(@invitation.token, subdomain: ENV["DEFAULT_SUBDOMAIN"]
+    @invitation_url = accept_invitation_url(@invitation.token, subdomain: ENV["DEFAULT_SUBDOMAIN"])
 
     mail(to: @invitation.email, subject: "Join #{invitation.team.name} in Spaces")
   end
