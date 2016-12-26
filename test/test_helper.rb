42f553b2375540cc5501fe9af9b0e15fc1a19c45
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-26 15:29:26 +0100

    remove duplications

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,13 @@ require "shoulda/context"
 require "capybara/poltergeist"
 require "shared/test_helpers/slack/identity"
 
+Capybara.configure do |config|
+  config.default_driver = :poltergeist
+  config.app_host = "http://#{ENV["DEFAULT_SUBDOMAIN"]}.lvh.me"
+  config.default_host = "#{ENV["DEFAULT_SUBDOMAIN"]}.lvh.me"
+  config.always_include_port = true
+end
+
 class ActiveSupport::TestCase
   fixtures :all
 end
@@ -19,20 +26,9 @@ class ActionDispatch::IntegrationTest
   include Capybara::DSL
   include Capybara::Assertions
 
-  setup do
-    Capybara.default_driver = :poltergeist
-    Capybara.app_host = "http://#{ENV["DEFAULT_SUBDOMAIN"]}.lvh.me"
-    Capybara.default_host = "#{ENV["DEFAULT_SUBDOMAIN"]}.lvh.me"
-    Capybara.always_include_port = true
-  end
-
   # Reset sessions and driver between tests
   # Use super wherever this method is redefined in your individual test classes
   def teardown
-    Capybara.app_host = nil
-    Capybara.default_host = "http://www.example.com"
-    Capybara.use_default_driver
-    Capybara.always_include_port = false
     Capybara.reset_sessions!
   end
 
