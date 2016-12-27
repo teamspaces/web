a88856be4d39a04b2b7c27747209e7e2d4c2c656
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-12-28 00:14:30 +0100

    move let inside describe

--- test/integration/login_register_funnel/email_login_test.rb
+++ test/integration/login_register_funnel/email_login_test.rb
@@ -3,10 +3,6 @@ require "test_helper"
 describe "Email Login", :integration do
   include TestHelpers::SubdomainHelper
 
-  let(:email_user) { users(:ulf) }
-  let(:user_with_several_teams) { users(:with_several_teams) }
-  let(:user_without_team) { users(:without_team) }
-
   def step_through_email_login_funnel_with(email, password, create_team)
     visit "/"
 
@@ -29,6 +25,9 @@ describe "Email Login", :integration do
   end
 
   describe "valid user authentication" do
+    let(:email_user) { users(:ulf) }
+    let(:user_with_several_teams) { users(:with_several_teams) }
+    let(:user_without_team) { users(:without_team) }
 
     def step_through_email_login_funnel_as(user, create_team=false)
       step_through_email_login_funnel_with(user.email, "password", create_team)
@@ -84,6 +83,8 @@ describe "Email Login", :integration do
   end
 
   describe "invalid user authentication" do
+    let(:email_user) { users(:ulf) }
+
     context "provided wrong password" do
       it "shows error messages" do
         step_through_email_login_funnel_with(email_user.email, "invalid_password", false)
