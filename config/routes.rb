19e6c7f367f32dd6ed0e19f24c7a6c28265133ea
Martin Samami <martin@digitalkookie.io>
2016-12-27 01:18:41 +0100

    Fix routing and some broken links

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
-
   constraints !ReservedSubdomain do
     resources :spaces do
       resources :pages, only: [:index, :new, :create]
     end
 
+    resource :team, only: [:show, :edit, :update, :destroy]
+
     resources :pages, only: [:show, :edit, :update, :destroy]
     resources :page_contents, only: [:show, :update]
 
     resources :invitations, only: [:index, :create, :destroy]
-    get 'slack_invitation', to: 'slack_invitations#create', as: :create_slack_invitation
+    get "slack_invitation", to: "slack_invitations#create", as: :create_slack_invitation
 
-    get :edit, to: 'teams#edit', as: :edit_team
-    patch '', to: 'teams#update'
-    delete '', to: 'teams#destroy'
-
-    resources :teams, only: [:show, :update, :destroy]
-
-    # get '', to: 'teams#show', as: :team
-    get '', to: "root_subdomain#index", as: :root_subdomain
+    get "/", to: "root_subdomain#index", as: :root_subdomain
   end
 
   resources :teams, only: [:index, :new, :create]
@@ -31,7 +25,7 @@ Rails.application.routes.draw do
     get :login, to: "devise/sessions#new", as: :new_user_session
     post :login, to: "devise/sessions#create", as: :user_session
     delete :logout, to: "devise/sessions#destroy", as: :destroy_user_session
-    get :sign_up, to: 'devise/registrations#new', as: :sign_up
+    get :sign_up, to: "devise/registrations#new", as: :sign_up
   end
 
   get :landing, to: "landing#index", as: :landing
