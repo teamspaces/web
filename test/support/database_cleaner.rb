608ac457e35250747ce2b9065d09d3d3378809cf
Martin Samami <martin@digitalkookie.io>
2016-12-27 21:28:18 +0100

    Only apply hacks unless CI

--- test/support/database_cleaner.rb
+++ test/support/database_cleaner.rb
@@ -1,27 +1,32 @@
 require "database_cleaner"
 
-DatabaseCleaner[:active_record].strategy = ENV["CI"] ? :truncation : :transaction
 DatabaseCleaner[:mongoid].strategy = :truncation
 
-class ActiveRecord::Base
-  mattr_accessor :shared_connection
-  @@shared_connection = nil
+if ENV["CI"]
+  DatabaseCleaner[:active_record].strategy = :truncation
+else
+  DatabaseCleaner[:active_record].strategy = :transaction
 
-  def self.connection
-    @@shared_connection || ConnectionPool::Wrapper.new(size: 1) { retrieve_connection }
+  class ActiveRecord::Base
+    mattr_accessor :shared_connection
+    @@shared_connection = nil
+
+    def self.connection
+      @@shared_connection || ConnectionPool::Wrapper.new(size: 1) { retrieve_connection }
+    end
   end
-end
 
-ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+  ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 
-# hack a mutex in the query execution so that we don't
-# get competing queries that can timeout and not get cleaned up
-module MutexLockedQuerying
-  @@semaphore = Mutex.new
+  # hack a mutex in the query execution so that we don't
+  # get competing queries that can timeout and not get cleaned up
+  module MutexLockedQuerying
+    @@semaphore = Mutex.new
 
-  def async_exec(*)
-    @@semaphore.synchronize { super }
+    def async_exec(*)
+      @@semaphore.synchronize { super }
+    end
   end
-end
 
-PG::Connection.prepend(MutexLockedQuerying)
+  PG::Connection.prepend(MutexLockedQuerying)
+end
