fb978cd43b9ecb124912008bd8e688810d8d1cd2
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-03 17:15:16 +0100

    change all context to describe

--- test/decorators/invitation_decorator_test.rb
+++ test/decorators/invitation_decorator_test.rb
@@ -7,13 +7,13 @@ describe InvitationDecorator, :model do
   let(:accepted_invitation) { invitations(:accepted_invitation).decorate }
 
   describe "#slack_invitation?" do
-    context "slack_invitation" do
+    describe "slack_invitation" do
       it "returns true" do
         assert_equal true, slack_invitation.slack_invitation?
       end
     end
 
-    context "email_invitation" do
+    describe "email_invitation" do
       it "returns false" do
         assert_equal false, email_invitation.slack_invitation?
       end
@@ -21,13 +21,13 @@ describe InvitationDecorator, :model do
   end
 
   describe "#email_invitation?" do
-    context "email_invitation" do
+    describe "email_invitation" do
       it "returns true" do
         assert_equal true, email_invitation.email_invitation?
       end
     end
 
-    context "slack_invitation" do
+    describe "slack_invitation" do
       it "returns false" do
         assert_equal false, slack_invitation.email_invitation?
       end
@@ -35,13 +35,13 @@ describe InvitationDecorator, :model do
   end
 
   describe "#already_accepted?" do
-    context "accepted invitation" do
+    describe "accepted invitation" do
       it "returns true" do
         assert_equal true, accepted_invitation.already_accepted?
       end
     end
 
-    context "open invitation" do
+    describe "open invitation" do
       it "returns false" do
         assert_equal false, email_invitation.already_accepted?
       end
@@ -49,13 +49,13 @@ describe InvitationDecorator, :model do
   end
 
   describe "#accepting_user_is_already_registered_using_email?" do
-    context "accepting user is already registered using email" do
+    describe "accepting user is already registered using email" do
       it "returns true" do
         assert_equal true, email_invitation.accepting_user_is_already_registered_using_email?
       end
     end
 
-    context "accepting user is not yet registered using email" do
+    describe "accepting user is not yet registered using email" do
       it "returns false" do
         assert_equal false, slack_invitation.accepting_user_is_already_registered_using_email?
       end
