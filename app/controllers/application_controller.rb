a1c6663c3ef8223d9d3be751abe588345b6d769d
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-04 12:14:51 +0100

    remove devise login related code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,11 @@ class ApplicationController < ActionController::Base
   include Pundit
   include HTTPBasicAuthentication
   include TokenParamLogin
-  include User::AfterSignInPath
-  include InvitationCookie
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
-  def after_sign_in_path_for(_resource)
-    user_after_sign_in_path
-  end
 
   def after_sign_out_path_for(_resource)
     root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
