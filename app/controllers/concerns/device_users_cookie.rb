c98c6562d9df5c71b1dd29d50eae4ace3ba1c7c8
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-05 22:57:07 +0100

    improve cookie class

--- app/controllers/concerns/device_users_cookie.rb
+++ app/controllers/concerns/device_users_cookie.rb
@@ -1,34 +1,35 @@
 class DeviceUsersCookie
 
+  COOKIE_NAME = :device_user_ids
+  COOKIE_DOMAIN = :all
+
   def initialize(cookies)
     @cookies = cookies
-    @cookie = cookies[:signed_in_user_ids]
 
-    @user_ids = []
-    @user_ids = JSON.parse(@cookie) if @cookie.present?
+    @user_ids = @cookies[COOKIE_NAME].present? ? JSON.parse(@cookies[COOKIE_NAME]) : []
   end
 
   def add(user)
     @user_ids << user.id if !@user_ids.include? user.id
-    save
+    save(@user_ids)
   end
 
   def remove(user)
     @user_ids.delete(user.id)
-    save
+    save(@user_ids)
   end
 
   def users
-    @user_ids.map { |user_id| User.find_by(id: user_id) }
+    @user_ids.map { |user_id| User.find_by(id: user_id) }.compact
   end
 
   def teams
-    users.map { |user| user.teams }.flatten.uniq
+    users.map(&:teams).flatten.uniq
   end
 
   private
 
-    def save
-      @cookies[:signed_in_user_ids] = { value: @user_ids.to_json, domain: :all,  tld_length: 2}
+    def save(user_ids)
+      @cookies[COOKIE_NAME] = { value: user_ids.to_json, domain: COOKIE_DOMAIN,  tld_length: 2}
     end
 end
