4f9b9b196c9da14a8154a971b0c1586f008b571e
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-06 15:15:40 +0100

    improve device users cookie test

--- test/controllers/concerns/device_users_cookie_test.rb
+++ test/controllers/concerns/device_users_cookie_test.rb
@@ -5,7 +5,7 @@ describe DeviceUsersCookie, :model do
   let(:subject) { DeviceUsersCookie.new({}) }
   let(:email_user) { users(:ulf) }
   let(:slack_user) { users(:slack_user_milad) }
-  let(:filled) do
+  let(:cookie_with_email_and_slack_user) do
     device_users_cookie = subject
     device_users_cookie.add(email_user)
     device_users_cookie.add(slack_user)
@@ -13,40 +13,40 @@ describe DeviceUsersCookie, :model do
   end
 
   describe "#add" do
-    it "adds user to stored" do
+    it "adds user device users" do
       assert_difference -> { subject.users.size }, 1 do
         subject.add(email_user)
       end
     end
 
-    context "user already stored" do
+    context "user already in device users" do
       it "does nothing" do
-        assert_difference -> { filled.users.size }, 0 do
-          filled.add(email_user)
+        assert_difference -> { cookie_with_email_and_slack_user.users.size }, 0 do
+          cookie_with_email_and_slack_user.add(email_user)
         end
       end
     end
   end
 
   describe "#remove" do
-    it "removes user from stored" do
-      assert_difference -> { filled.users.size }, -1 do
-        filled.remove(email_user)
+    it "removes user from device users" do
+      assert_difference -> { cookie_with_email_and_slack_user.users.size }, -1 do
+        cookie_with_email_and_slack_user.remove(email_user)
       end
     end
   end
 
   describe "#users" do
-    it "returns stored users" do
-      assert_equal [email_user, slack_user], filled.users
+    it "returns device users" do
+      assert_equal [email_user, slack_user], cookie_with_email_and_slack_user.users
     end
   end
 
   describe "#teams" do
-    it "returns stored users teams" do
-      teams = (email_user.teams + slack_user.teams).uniq
+    it "returns device users teams" do
+      device_users_teams = (email_user.teams + slack_user.teams).uniq
 
-      assert_equal teams, filled.teams
+      assert_equal device_users_teams, cookie_with_email_and_slack_user.teams
     end
   end
 end
