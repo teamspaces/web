b8b51c1a78fc8fb3fe05d8536a94420d2e9976cf
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-07 12:03:51 +0100

    extract user accept invitation url

--- app/controllers/concerns/login_register_funnel/sign_in_path_for_user.rb
+++ app/controllers/concerns/login_register_funnel/sign_in_path_for_user.rb
@@ -1,4 +1,5 @@
 module LoginRegisterFunnel::SignInPathForUser
+  include LoginRegisterFunnel::UserAcceptInvitationPath
   extend ActiveSupport::Concern
 
   def sign_in_path_for(user)
@@ -6,7 +7,7 @@ module LoginRegisterFunnel::SignInPathForUser
     sign_in_path_helper = UserSignInPathHelper.new(user, self)
 
     case
-      when invitation_present? then sign_in_path_helper.accept_invitation_url
+      when invitation_present? then user_accept_invitation_path(user)
       when shared_user_info.user_wants_to_create_team? then sign_in_path_helper.create_team_url
       else sign_in_path_helper.url_depending_on_user_teams_count
     end
@@ -15,6 +16,10 @@ module LoginRegisterFunnel::SignInPathForUser
   private
 
     def invitation_present?
-      LoginRegisterFunnel::InvitationCookie.new(cookies).invitation.present?
+      invitation_cookie.invitation.present?
+    end
+
+    def invitation_cookie
+      @invitation_cookie ||= LoginRegisterFunnel::InvitationCookie.new(cookies)
     end
 end
---
+++ app/controllers/concerns/login_register_funnel/user_accept_invitation_path.rb
@@ -0,0 +1,31 @@
+module LoginRegisterFunnel::UserAcceptInvitationPath
+  extend ActiveSupport::Concern
+
+  def user_accept_invitation_path(user)
+    invitation = invitation_cookie.invitation
+    invitation_cookie.delete
+
+    if invitation.present?
+      policy = User::AcceptInvitationPolicy.new(user, invitation)
+
+      if policy.matching?
+        User::AcceptInvitation.call(user: user, invitation: invitation)
+
+        flash[:notice] = t("successfully_accepted_invitation")
+        team_url(subdomain: invitation.team.subdomain, auth_token: GenerateLoginToken.call(user: user))
+      else
+        flash[:notice] = t("invitation_does_not_match_user")
+        sign_in_path_for(user)
+      end
+    else
+      flash[:notice] = t("invitation_does_not_exist")
+      sign_in_path_for(user)
+    end
+  end
+
+  private
+
+    def invitation_cookie
+      @invitation_cookie ||= LoginRegisterFunnel::InvitationCookie.new(cookies)
+    end
+end
