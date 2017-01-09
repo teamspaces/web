01f5f960e5f0dd5ea4677bc039ca1c868abc059f
Martin Samami <martin@digitalkookie.io>
2017-01-09 01:41:50 +0100

    Make it work baby

---
+++ app/presenters/editor_settings_hash_presenter.rb
@@ -0,0 +1,45 @@
+class EditorSettingsHashPresenter
+  DEFAULT_COLLECTION = 'collab_pages'
+  DEFAULT_EXPIRES_IN = ENV["EDITOR_SECONDS_TO_JWT_EXPIRY"] || 10.seconds
+
+  def initialize(controller:, user:, page:, expires_in: DEFAULT_EXPIRES_IN, collection: DEFAULT_COLLECTION)
+    @controller = controller
+    @user = user
+    @page = page
+    @expires_in = expires_in
+    @collection = collection
+  end
+
+  def to_hash
+    {
+      collection: @collection,
+      document_id: format_document_id,
+      collab_url: "#{ENV["COLLAB_SERVICE_URL"]}?token=#{generate_token}",
+      page_content_url: @controller.page_content_url(@page.page_content),
+      csrf_token: @controller.view_context.form_authenticity_token
+    }
+  end
+
+  private
+
+    def payload
+      {
+        exp: format_expires_in,
+        user_id: @user.id,
+        collection: @collection,
+        document_id: format_document_id
+      }
+    end
+
+    def generate_token
+      JWT.encode(payload, ENV["COLLAB_SERVICE_JWT_SECRET"], "HS256")
+    end
+
+    def format_expires_in
+      Time.now.to_i + @expires_in
+    end
+
+    def format_document_id
+      @page.id.to_s # integers not supported by sharedb so we're faking it
+    end
+end
---
+++ test/presenters/editor_settings_hash_presenter_test.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+
+describe EditorSettingsHashPresenter, :model do
+  subject do
+    EditorSettingsHashPresenter.new(controller: controller_mock,
+                                    user: user,
+                                    page: page)
+  end
+
+  let(:user) { users(:ulf) }
+  let(:page) { pages(:spaces) }
+  let(:controller_mock) do
+    controller_mock = mock
+    controller_mock.stubs(:collab_url).returns("fake_collab_url")
+    controller_mock.stubs(:page_content_url).returns("fake_page_content_url")
+
+    view_context_mock = mock
+    view_context_mock.stubs(:form_authenticity_token).returns("fake_token")
+
+    controller_mock.stubs(:view_context).returns(view_context_mock)
+
+    controller_mock
+  end
+
+  describe "#to_hash" do
+    it "formats correctly" do
+      hash = subject.to_hash
+
+      assert_equal "collab_pages", hash[:collection]
+
+      assert_not_nil hash[:document_id]
+      assert_instance_of String, hash[:document_id]
+
+      assert_match /ws\:\/\/.*.token=.*/, hash[:collab_url]
+
+      assert_equal "fake_page_content_url", hash[:page_content_url]
+      assert_equal "fake_token", hash[:csrf_token]
+    end
+  end
+end
