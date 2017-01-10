30e7c2539b01f0127b038c20aafce70a2855f673
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-10 15:42:59 +0100

    edit account information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < SubdomainBaseController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = current_user
+      @user = current_user.decorate
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
