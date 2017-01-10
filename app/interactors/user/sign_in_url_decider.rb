6b0b9badd34e042d37cbbf14bd0b58bcfd1567ba
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-10 14:56:24 +0100

    change order of deciding over sign in url

--- app/interactors/user/sign_in_url_decider.rb
+++ app/interactors/user/sign_in_url_decider.rb
@@ -12,8 +12,8 @@ class User::SignInUrlDecider
   def decide_url
     case
       when invitation_present? then user_accept_invitation_url
-      when team_creation_requested? then sign_in_url_for_user.create_team_url
       when redirect_to_team? then sign_in_url_for_user.team_url(@team_to_redirect_to)
+      when team_creation_requested? then sign_in_url_for_user.create_team_url
       else sign_in_url_for_user.url_depending_on_user_teams_count
     end
   end
