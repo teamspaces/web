b14e70a9e8581db5078a7a32de7e3312bc26b91e
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-11 13:07:35 +0100

    do processing in background job

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,13 @@ class User < ApplicationRecord
   before_create :generate_avatar
 
   def generate_avatar
+    attacher = Shrine::AvatarUploader::Attacher.new(self, :avatar)
     img = Avatarly.generate_avatar(self.name)
-    t = Tempfile.new("test_temp.png",  :encoding => 'ascii-8bit')
-    t.write(img)
-    t.close
-    self.avatar = File.new(t)
-    t.delete
+    temp_file = Tempfile.new("avatar_temp.png", encoding: "ascii-8bit")
+    temp_file.write(img)
+    attacher.assign(temp_file)
+    temp_file.close
+    temp_file.delete
   end
 
   def name=(name)
