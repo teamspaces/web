f2caa8603ed2097b1b3638ff0138468b195407ed
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-11 22:00:04 +0100

    generate avatar

--- app/interactors/user/attach_default_avatar.rb
+++ app/interactors/user/attach_default_avatar.rb
@@ -1,21 +1,19 @@
-
 class User::AttachDefaultAvatar
   include Interactor
 
   def call
+    @user = context.user
 
+    action
   end
- # before_create :generate_avatar
 
-
- # def generate_avatar
- #   attacher = Shrine::AvatarUploader::Attacher.new(self, :avatar)
- #   img = Avatarly.generate_avatar(self.name)
- #   temp_file = Tempfile.new("avatar_temp.png", encoding: "ascii-8bit")
- #   temp_file.write(img)
- #   attacher.assign(temp_file)
- #   temp_file.close
- #   temp_file.delete
- # end
-
+  def action
+    attacher = Shrine::AvatarUploader::Attacher.new(@user, :avatar)
+    img = Avatarly.generate_avatar(@user.name)
+    temp_file = Tempfile.new("avatar_temp.png", encoding: "ascii-8bit")
+    temp_file.write(img)
+    attacher.assign(temp_file)
+    temp_file.close
+    temp_file.delete
+  end
 end
