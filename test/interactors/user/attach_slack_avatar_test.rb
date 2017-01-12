2dc23384110e9620dc1b0a975ae151f501c5890b
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-12 21:13:19 +0100

    test attach slack avatar interactor

---
+++ test/interactors/user/attach_slack_avatar_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+describe User::AttachSlackAvatar, :model do
+  let(:user) { users(:lars) }
+
+  it "assigns generated avatar" do
+    User::AttachSlackAvatar.call(user: user, slack_identity: TestHelpers::Slack.identity(:unknown_user))
+
+    assert user.avatar.present?
+    assert user.slack_avatar?
+  end
+end
