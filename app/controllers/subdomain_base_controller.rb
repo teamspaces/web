600be8126360f12a80399cf749a580cafbf7a4bf
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-14 23:00:56 +0100

    integrate code into devise failure app

--- app/controllers/subdomain_base_controller.rb
+++ app/controllers/subdomain_base_controller.rb
@@ -10,28 +10,8 @@ class SubdomainBaseController < ApplicationController
 
     def check_team_membership
       unless current_team && TeamPolicy.new(pundit_user, current_team).read?
-        user = user_trying_to_login_on_team_subdomain
-
-        redirect_to case
-          when user.nil? then root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
-          when user.login_using_slack? then team_slack_login_path_for(team: subdomain_team)
-          when user.login_using_email?
-            complete_login_register_funnel_review_email_address_step_for(user: user)
-            new_email_login_path
-        end
+        redirect_to root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
       end
-    end
-
-    def user_trying_to_login_on_team_subdomain
-      subdomain_team.users.find_by(id: available_users.users)&.decorate
-    end
-
-    def team_slack_login_path_for(team:)
-      user_slack_omniauth_authorize_url(subdomain: ENV["DEFAULT_SUBDOMAIN"], state: :login, team_id: team.id)
-    end
-
-    def complete_login_register_funnel_review_email_address_step_for(user:)
-      shared_user_info.reviewed_email_address = user.email
     end
 
     def pundit_user
--- app/lib/custom_warden_failure.rb
+++ app/lib/custom_warden_failure.rb
@@ -1,6 +1,18 @@
 class CustomWardenFailure < Devise::FailureApp
   def redirect_url
-    root_url(subdomain:  ENV["DEFAULT_SUBDOMAIN"])
+    if on_team_subdomain?
+      user = user_trying_to_login_on_team_subdomain
+
+      redirect_to case
+        when user.nil? then root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
+        when user.login_using_slack? then team_slack_login_path_for(team: subdomain_team)
+        when user.login_using_email?
+          complete_login_register_funnel_review_email_address_step_for(user: user)
+          new_email_login_path
+        end
+    else
+      redirect_to root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
+    end
   end
 
   # You need to override respond to eliminate recall
@@ -10,5 +22,29 @@ class CustomWardenFailure < Devise::FailureApp
     else
       redirect
     end
+  end
+
+  def user_trying_to_login_on_team_subdomain
+    subdomain_team.users.find_by(id: available_users.users)&.decorate
+  end
+
+  def team_slack_login_path_for(team:)
+    user_slack_omniauth_authorize_url(subdomain: ENV["DEFAULT_SUBDOMAIN"], state: :login, team_id: team.id)
+  end
+
+  def complete_login_register_funnel_review_email_address_step_for(user:)
+    shared_user_info.reviewed_email_address = user.email
+  end
+
+  def on_team_subdomain?
+    subdomain_team.present?
+  end
+
+  def subdomain_team
+    @subdomain_team ||= Team.find_by(subdomain: request.subdomain)
+  end
+
+  def available_users
+    @available_users ||= LoginRegisterFunnel::BaseController::AvailableUsersCookie.new(request.cookies)
   end
 end
