4ee95fc6fde4cd5dc61433e27f66a87772ff82ea
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-17 13:14:59 +0100

    remove delete promoted plugin

--- app/models/team_logo_uploader.rb
+++ app/models/team_logo_uploader.rb
@@ -6,7 +6,7 @@ class TeamLogoUploader < Shrine
   plugin :default_url
   plugin :add_metadata
   plugin :remove_invalid
-  plugin :delete_promoted
+  #plugin :delete_promoted
   plugin :cached_attachment_data # enables caching the form
   plugin :determine_mime_type # determines MIME type from file content
   plugin :validation_helpers, default_messages: {
