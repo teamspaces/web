dab1b022af4c210be252587418ce69bf3fc5ab07
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-17 17:43:24 +0100

    add generate team logos disposables

---
+++ cli/disposables/generate_team_logos.rb
@@ -0,0 +1,24 @@
+require_relative "../cli_helper"
+
+module CLI
+  module Disposables
+    class GenerateTeamLogos < Thor
+
+      desc "generate", "save generated logos for teams without logos"
+      def generate
+        i = 0
+
+        Team.where(logo_data: nil).find_each do |team|
+          Team::Logo::AttachGeneratedLogo.call(team: team)
+          team.save
+          i += 1
+        end
+
+        puts "Generated logos for #{i} teams"
+      end
+
+    end
+  end
+end
+
+CLI::Disposables::GenerateTeamLogos.start(ARGV)
