8b87152ebb3b8cb12ee8cd5323a368fc7dd13097
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-19 18:58:59 +0100

    add cache control

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -9,11 +9,13 @@ when "s3"
     access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
     secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region:            ENV["AWS_REGION"],
-    bucket:            ENV["S3_FILES_BUCKET"]
+    bucket:            ENV["S3_FILES_BUCKET"],
+    cache_control:     "public, max-age=#{30.days}",
+    acl:               "public-read"
   }
 
-  { cache: Shrine::Storage::S3.new(prefix: "cache", upload_options: { acl: "public-read" }, **s3_options),
-    store: Shrine::Storage::S3.new(prefix: "store", upload_options: { acl: "public-read" }, **s3_options) }
+  { cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+    store: Shrine::Storage::S3.new(prefix: "store", **s3_options) }
 else # default to 'local'
   { cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
     store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store") }
