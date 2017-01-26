d5ecd80e61420b0901db7dda22bb6271baa449a2
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-26 10:09:58 +0100

    update test

--- test/interactors/user/email/send_confirmation_instructions_test.rb
+++ test/interactors/user/email/send_confirmation_instructions_test.rb
@@ -8,10 +8,7 @@ describe User::Email::SendConfirmationInstructions, :controller do
     mailer_mock.stubs(:deliver_later).returns(true)
     mailer_mock
   end
-  before(:each) do
-    sign_in unconfirmed_user
-    get team_url(subdomain: unconfirmed_user.teams.first.subdomain)
-  end
+  before(:each) { sign_in unconfirmed_user }
 
   describe "#call" do
     describe "current_url can be used as email link for confirmation" do
@@ -21,11 +18,24 @@ describe User::Email::SendConfirmationInstructions, :controller do
                                 {confirmation_url: "http://spaces.example.com/team?confirmation_token=#{unconfirmed_user.confirmation_token}"})
                           .returns(mailer_mock)
 
-        assert subject.call(user: unconfirmed_user, controller: @controller).success?
+        get team_url(subdomain: unconfirmed_user.teams.first.subdomain)
+      end
+    end
+
+    describe "current_url can't be used as email link for confirmation" do
+      it "sends confirmation mail, with link to subdomain root url" do
+        CustomDeviseMailer.expects(:confirmation_instructions)
+                          .with(unconfirmed_user, unconfirmed_user.confirmation_token,
+                                {confirmation_url: "http://spaces.example.com/?confirmation_token=#{unconfirmed_user.confirmation_token}"})
+                          .returns(mailer_mock)
+
+        post user_email_confirmation_url(subdomain: unconfirmed_user.teams.first.subdomain)
       end
     end
 
     it "saves email sent at time" do
+      get team_url(subdomain: unconfirmed_user.teams.first.subdomain)
+
       assert subject.call(user: unconfirmed_user, controller: @controller).success?
       unconfirmed_user.reload
 
