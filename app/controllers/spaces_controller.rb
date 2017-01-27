eb29c14e686c849205460b80b79fa9f0d7efb28b
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-27 13:21:24 +0100

    cut space covers to quadratic

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -84,6 +84,6 @@ class SpacesController < SubdomainBaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def space_params
-      params.require(:space).permit(:name, :cover)
+      params.require(:space).permit(:name, :cover).to_h
     end
 end
