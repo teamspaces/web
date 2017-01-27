a65fc38f4e5f2c81117fa49e05941111c56670c4
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-27 17:15:24 +0100

    test space form

--- test/forms/space/form_test.rb
+++ test/forms/space/form_test.rb
@@ -1,12 +1,33 @@
 require "test_helper"
 
 describe Space::Form, :model do
+  let(:space) { spaces(:spaces) }
+
+  subject { Space::Form.new(space: space) }
 
   describe "validations" do
-    should validate_presence_of(:user)
     should validate_presence_of(:name)
     should validate_presence_of(:team_id)
   end
 
+  describe "space cover" do
+    let(:uploaded_cover_file) { "uploaded_cover_file" }
+
+    it "attaches uploaded cover" do
+      Space::Cover::AttachUploadedCover.expects(:call)
+                                       .with(space: space, file: uploaded_cover_file)
+                                       .returns(true)
+
+      subject.cover = uploaded_cover_file
+    end
+  end
+
+  describe "#save" do
+    it "saves space" do
+      assert Space::Form.new(space: space, params: { name: "new_name" }).save
+
+      assert_equal "new_name", space.name
+    end
+  end
 end
 
