77d3021e10c1d8ff3f7284efa570cbcadb5666c9
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-27 12:04:05 +0100

    uploader inherit from base uploader

--- app/models/team_logo_uploader.rb
+++ app/models/team_logo_uploader.rb
@@ -1,23 +1,4 @@
-class TeamLogoUploader < Shrine
-  plugin :logging
-  plugin :versions # save different avatar versions
-  plugin :delete_raw # delete processed files after uploading
-  plugin :processing
-  plugin :default_url
-  plugin :add_metadata
-  plugin :remove_invalid
-  plugin :cached_attachment_data # enables caching the form
-  plugin :determine_mime_type # determines MIME type from file content
-  plugin :validation_helpers, default_messages: {
-    mime_type_inclusion: ->(list) { I18n.t("errors.file.mime_type_inclusion", list: list.join(", ")) },
-    max_size: ->(max) { I18n.t("errors.file.max_size", max: max.to_f/1024/1024) }
-  }
-
-  Attacher.validate do
-    validate_mime_type_inclusion %w[image/jpeg image/jpg image/png]
-    validate_max_size 8*1024*1024
-  end
-
+class TeamLogoUploader < BaseUploader
   def generate_location(io, context)
     type  = "team_logo"
     version = context[:version] if context[:version]
