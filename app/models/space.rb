f54592f33598ff574f22d6b4871e7978e85eb76c
EmmanuelSTs <stadleremmanuel1@gmail.com>
2017-01-27 23:54:34 +0100

    Add Space cover images (#180)
    * add space cover uploader
    
    * add form for create and update
    
    * upload attached cover
    
    * update controller
    
    * update view
    
    * update form
    
    * uploader inherit from base uploader
    
    * show cover in view
    
    * move methods to parent class
    
    * change paths to default images
    
    * cut space covers to quadratic
    
    * add form test
    
    * test space form
    
    * test spaces controller
    
    * improve
    
    * public uploads
    
    * custom s3 uploader
    
    * shorter version
    
    * update test
    
    * revert
    
    * Took the liberty to update the images
    
    * Reset accidental commit
    
    * Add a comment

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,6 @@
 class Space < ApplicationRecord
+  include SpaceCoverUploader[:cover]
+
   has_many :pages, dependent: :destroy
   belongs_to :team
   validates :team, presence: true
---
+++ app/models/space_cover.rb
@@ -0,0 +1,5 @@
+class SpaceCover
+
+  SIZES = [[1020, 680], [450, 300], [30, 20]]
+
+end
