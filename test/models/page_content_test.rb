6e0f5d27174ddf14e70f9ce42708e12aab76e993
Martin Samami <martin@digitalkookie.io>
2017-01-28 01:56:03 +0100

    Test update_word_count

--- test/models/page_content_test.rb
+++ test/models/page_content_test.rb
@@ -7,4 +7,13 @@ describe PageContent do
   it "must be valid" do
     value(page_content).must_be :valid?
   end
+
+  describe "#update_word_count" do
+    it "updates Page#word_count" do
+      page_content.contents = "Here is some hot and spicy text for you."
+      page_content.save
+
+      assert_equal 9, page.word_count
+    end
+  end
 end
