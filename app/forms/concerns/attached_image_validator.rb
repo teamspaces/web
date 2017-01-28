98d84f32793c6c353bd48826da36e16595015760
Martin Samami ッ <martin@digitalkookie.io>
2017-01-28 15:08:28 +0100

    Update comments

--- app/forms/concerns/attached_image_validator.rb
+++ app/forms/concerns/attached_image_validator.rb
@@ -1,7 +1,9 @@
 class AttachedImageValidator < ActiveModel::EachValidator
-  # validates shirne attacher, for example team logos or space covers
-  # validates :logo, attached_image: true
 
+  # Validates Shrine attacher eg. a team logo or space cover.
+  # 
+  # Example:
+  #   validates :logo, attached_image: true
   def validate_each(record, attribute, value)
     record.instance_variable_get("@#{record.model_name.to_str.downcase}")
           .send("#{attribute}_attacher").errors.each do |message|
