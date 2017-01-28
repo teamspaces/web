932370085071d31438a073c990e72f3c802b1216
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-28 11:50:03 +0100

    add comment

--- app/forms/concerns/attached_image_validator.rb
+++ app/forms/concerns/attached_image_validator.rb
@@ -1,4 +1,7 @@
 class AttachedImageValidator < ActiveModel::EachValidator
+  # validates shirne attacher, for example team logos or space covers
+  # validates :logo, attached_image: true
+
   def validate_each(record, attribute, value)
     record.instance_variable_get("@#{record.model_name.to_str.downcase}")
           .send("#{attribute}_attacher").errors.each do |message|
