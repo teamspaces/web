b8b3c36afd076040287eee9349ffe9c39168e88a
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-29 11:24:37 +0100

    correct test

--- test/controllers/invitations_controller_test.rb
+++ test/controllers/invitations_controller_test.rb
@@ -20,8 +20,7 @@ describe InvitationsController do
   describe "#create" do
     it "creates an invitation" do
       assert_difference -> { Invitation.count }, 1 do
-        params = { send_invitation_form: { email: "gallen@nl.se"} }
-        post team_invitations_url, params: params
+        post team_invitations_url, params: { invitation: { email: "gallen@nl.se"} }
       end
     end
 
@@ -29,8 +28,7 @@ describe InvitationsController do
 
       it "does not create the invitation" do
         assert_difference -> { Invitation.count }, 0 do
-          params = { send_invitation_form: { email: "invalid_email"} }
-          post team_invitations_url, params: params
+          post team_invitations_url, params: { invitation: { email: "invalid_email"} }
         end
       end
     end
