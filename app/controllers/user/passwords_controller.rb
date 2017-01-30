360db6c7c90227be348dbee05d4acdf398eba292
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-30 15:24:09 +0100

    remove new password view

--- app/controllers/user/passwords_controller.rb
+++ app/controllers/user/passwords_controller.rb
@@ -1,16 +1,10 @@
 class User::PasswordsController < Devise::PasswordsController
 
-  # overwrite unnecessary devise action
-  def new
-    redirect_to root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
-  end
-
   def after_sending_reset_password_instructions_path_for(resource_name)
-    new_login_register_funnel_after_reset_password_path
+    new_login_register_funnel_after_reset_password_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
   end
 
   def after_resetting_password_path_for(resource)
     sign_in(resource)
   end
-
 end
