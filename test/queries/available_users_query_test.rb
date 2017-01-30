0012e47ce119c70e805ddbd9939bf33b45389bf1
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-30 20:05:42 +0100

    update test

--- test/queries/available_users_query_test.rb
+++ test/queries/available_users_query_test.rb
@@ -4,6 +4,7 @@ describe AvailableUsersQuery, :model do
   let(:email_user) { users(:lars) }
   let(:slack_user) { users(:slack_user_milad) }
   let(:with_one_space) { users(:with_one_space) }
+  before(:each) {  Authie::Session.delete_all }
 
   subject { AvailableUsersQuery.new(1) }
 
@@ -12,6 +13,7 @@ describe AvailableUsersQuery, :model do
   end
 
   describe "#users" do
+
     it "only returns users of browser" do
       create_session(user: email_user,     browser_id: 1)
       create_session(user: slack_user,     browser_id: 1)
@@ -31,7 +33,23 @@ describe AvailableUsersQuery, :model do
       create_session(user: email_user, team_id: 1)
       create_session(user: email_user, team_id: 2)
 
-      assert_equal subject.users.length == 1
+      assert subject.users.length == 1
+    end
+  end
+
+  describe "#teams" do
+    it "returns all teams of active users" do
+      create_session(user: email_user)
+      create_session(user: with_one_space)
+      create_session(user: team_members(:maja_at_power).user, active: false)
+
+      subject.teams
+    end
+  end
+
+  describe "#user_signed_in_on_another_subdomain" do
+    it "returns the team user who is already signed in on another team subdomain" do
+
     end
   end
 end
