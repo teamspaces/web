79236d3e350a58263d283ff5aad6e4dddc225415
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-30 15:45:49 +0100

    sign in user automatically after email update

--- app/controllers/user/passwords_controller.rb
+++ app/controllers/user/passwords_controller.rb
@@ -3,8 +3,4 @@ class User::PasswordsController < Devise::PasswordsController
   def after_sending_reset_password_instructions_path_for(resource_name)
     new_login_register_funnel_after_reset_password_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
   end
-
-  def after_resetting_password_path_for(resource)
-    sign_in(resource)
-  end
 end
