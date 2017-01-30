c02c8b05ebb6d21f78fef95ae6c385e0dec61f33
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-30 17:47:23 +0100

    update mail previews

--- test/mailers/previews/custom_devise_mailer_preview.rb
+++ test/mailers/previews/custom_devise_mailer_preview.rb
@@ -1,5 +1,17 @@
 class CustomDeviseMailerPreview < ActionMailer::Preview
   def confirmation_instructions
-    CustomDeviseMailer.confirmation_instructions(User.last, "faketoken", {})
+    user = User.last
+
+    CustomDeviseMailer.confirmation_instructions(user, user.confirmation_token, {confirmation_url: "www.lvh.me"})
+  end
+
+  def reset_password_instructions
+    user = User.last
+
+    CustomDeviseMailer.reset_password_instructions(user, user.reset_password_token, {})
+  end
+
+  def password_change
+    CustomDeviseMailer.password_change(User.last, {})
   end
 end
