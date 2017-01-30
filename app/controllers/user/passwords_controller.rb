3598bea52edaa1475d27e008f44a9f6594a38130
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-30 18:33:08 +0100

    update

--- app/controllers/user/passwords_controller.rb
+++ app/controllers/user/passwords_controller.rb
@@ -3,4 +3,8 @@ class User::PasswordsController < Devise::PasswordsController
   def after_sending_reset_password_instructions_path_for(resource_name)
     new_login_register_funnel_after_sent_reset_password_instruction_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
   end
+
+  def after_resetting_password_path_for(resource)
+    sign_in(resource)
+  end
 end
--- test/mailers/previews/custom_devise_mailer_preview.rb
+++ test/mailers/previews/custom_devise_mailer_preview.rb
@@ -1,14 +1,10 @@
 class CustomDeviseMailerPreview < ActionMailer::Preview
   def confirmation_instructions
-    user = User.last
-
-    CustomDeviseMailer.confirmation_instructions(user, user.confirmation_token, {confirmation_url: "www.lvh.me"})
+    CustomDeviseMailer.confirmation_instructions(User.last, "fake_token", {confirmation_url: "www.lvh.me"})
   end
 
   def reset_password_instructions
-    user = User.last
-
-    CustomDeviseMailer.reset_password_instructions(user, user.reset_password_token, {})
+    CustomDeviseMailer.reset_password_instructions(User.last, "fake_token",, {})
   end
 
   def password_change
