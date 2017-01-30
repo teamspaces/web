0f6f39077b8964c46919ad5e5a115a8af012e55b
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-01-30 14:55:00 +0100

    change routes

--- app/controllers/user/passwords_controller.rb
+++ app/controllers/user/passwords_controller.rb
@@ -1,11 +1,16 @@
 class User::PasswordsController < Devise::PasswordsController
 
+  # overwrite unnecessary devise action
+  def new
+    redirect_to root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"])
+  end
+
   def after_sending_reset_password_instructions_path_for(resource_name)
     new_login_register_funnel_after_reset_password_path
   end
 
   def after_resetting_password_path_for(resource)
-    new_login_register_funnel_after_reset_password_path
+    sign_in(resource)
   end
 
 end
