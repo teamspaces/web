38c5b57f0573937b805d3953a6b1c89d43c5c677
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-02 13:47:20 +0100

    update test

--- test/feature/login_register_funnel/reset_password_test.rb
+++ test/feature/login_register_funnel/reset_password_test.rb
@@ -20,8 +20,8 @@ describe "Reset Password", :capybara do
       assert_content "We would like to send you an email"
 
       CustomDeviseMailer.expects(:reset_password_instructions)
-                        .with(includes(email_user))
-                        .returns(true)
+                        .returns(email_mock = mock)
+      email_mock.expects(:deliver_later)
 
       click_on "This is my email, send me the instructions please"
 
