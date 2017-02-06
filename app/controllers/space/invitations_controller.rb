0273841db2839c3f88b9311aea69b9f973a215d2
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-06 09:46:35 +0100

    remove additional actions

--- app/controllers/space/invitations_controller.rb
+++ app/controllers/space/invitations_controller.rb
@@ -4,29 +4,6 @@ class Space::InvitationsController < SubdomainBaseController
 
   layout 'client'
 
-  # GET /spaces/:space_id/members
-  def index
-    @space_team_members = @space.team_members
-    @team_members = @space.team.members
-  end
-
-  # POST /spaces/:space_id/members
-  def create
-    @space_member = SpaceMember.new(space: @space, team_member_id: space_member_params[:team_member_id])
-
-    authorize @space_member, :create?
-
-    respond_to do |format|
-      if @space_member.save
-        format.html { redirect_to space_members_path(@space) }
-        format.json { render :show, status: :created, location: @space_member }
-      else
-        format.html { render :new }
-        format.json { render json: @space_member.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /spaces/:space_id/members/:id
   def destroy
     authorize @space_member, :destroy?
