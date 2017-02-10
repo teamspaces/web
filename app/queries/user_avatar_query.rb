564a3f60b9f1e9f612cd56e7e25f2da74258e918
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-10 20:08:04 +0100

    update

--- app/queries/user_avatar_query.rb
+++ app/queries/user_avatar_query.rb
@@ -1,32 +1,30 @@
 class UserAvatarQuery
 
-   AVATAR_USERS_TO_SHOW = 3
+  attr_reader :users_count_to_return, :relation
 
+  def initialize(users_count_to_return:, _for: relation)
+    @users_count_to_return = users_count_to_return
+    @relation = relation
+  end
 
-     helper_method :avatar_users
   def avatar_users
-
+    User.where(id: limited_sample_relation_user_ids).all
   end
 
-  helper_method :number_of_unseen_avatars
-  def number_of_unseen_avatars
-    @number_of_unseen_avatars ||=
-      [(current_team.users.count - AVATAR_USERS_TO_SHOW), 0].max
+  def count_of_not_returned_avatar_users
+    [(relation_users.count - users_count_to_return), 0].max
   end
 
+  private
 
-  def for_team(team)
-    user_ids = team.users
-                   .limit(100)
-                   .pluck(:id)
-                   .sample(AVATAR_USERS_TO_SHOW)
+    def relation_users
+      relation.users
+    end
 
-    User.where(id: @avatar_user_ids).all
-  end
-
-
-  def for_space(space)
-
-  end
+    def limited_sample_relation_user_ids
+      relation_users.limit(100)
+                    .pluck(:id)
+                    .sample(users_count_to_return)
+    end
 
 end
