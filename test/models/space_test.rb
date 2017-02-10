8901b8da308672fae62e50a5feedba1d8a5ff8b8
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-10 22:12:14 +0100

    add test

--- test/models/space_test.rb
+++ test/models/space_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 describe Space do
   should have_many(:pages).dependent(:destroy)
+  should have_many(:users)
   should belong_to(:team)
   should validate_presence_of(:team)
 end
