42cc3aadc9866c0853c9996bc914ea44ec81021e
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-11 19:36:20 +0100

    update

--- app/policies/page_policy.rb
+++ app/policies/page_policy.rb
@@ -13,11 +13,12 @@ class PagePolicy
   end
 
   def index?
-    user_is_allowed_to_access_space?
+    team == space.team
+    #user_is_allowed_to_access_space?
   end
 
   def team_page?
-    team == page.team && user_is_allowed_to_access_space?
+    team == page.team #&& user_is_allowed_to_access_space?
   end
 
   alias_methods :team_page?, [:show?, :new?, :edit?, :update?, :destroy?]
