e70fe378075ca75733f7362dfe8cb2fe42123489
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-11 12:13:32 +0100

    space query space policy, invlaid

--- app/policies/space_policy/scope.rb
+++ app/policies/space_policy/scope.rb
@@ -1,12 +1,25 @@
 class SpacePolicy::Scope
-  attr_reader :team, :scope
+  attr_reader :team, :user, :scope
 
   def initialize(default_context, scope)
     @team = default_context.team
+    @user = default_context.user
+
     @scope = scope
   end
 
   def resolve
-    scope.where(team: team)
+    scope.where(team: team, access_control_rule: Space::AccessControlRules::TEAM)
+         .or(private_user_spaces)
+  end
+
+  def private_user_spaces
+    Space.joins(:space_members)
+         .where(team: team,
+                access_control_rule: Space::AccessControlRules::PRIVATE,
+                space_members: { team_member: user})
   end
 end
+
+
+
