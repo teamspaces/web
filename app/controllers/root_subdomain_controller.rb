26c0ac069d63e624f902035107a6d799f043751d
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-13 14:37:39 +0100

    filter spaces

--- app/controllers/root_subdomain_controller.rb
+++ app/controllers/root_subdomain_controller.rb
@@ -1,11 +1,11 @@
 class RootSubdomainController < SubdomainBaseController
   def index
     #update nicht mehr nach team sondern user
-    case current_team.spaces.count
+    case policy_scope(Space).count
     when 0
       redirect_to new_space_path
     when 1
-      redirect_to space_pages_path(current_team.spaces.first)
+      redirect_to space_pages_path(policy_scope(Space).first)
     else
       redirect_to spaces_path
     end
