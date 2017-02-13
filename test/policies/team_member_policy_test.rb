aa03421bc81f2b014158045e8bde63209ae3043f
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-13 21:06:04 +0100

    refine tests

--- test/policies/team_member_policy_test.rb
+++ test/policies/team_member_policy_test.rb
@@ -3,35 +3,31 @@ require "test_helper"
 describe TeamMemberPolicy, :model do
   let(:user) { users(:lars) }
   let(:team) { teams(:spaces) }
+  let(:user_team_member) { team_members(:lars_at_spaces) }
+  let(:normal_member) { team_members(:maja_at_spaces) }
+  let(:primary_owner) { team_members(:ulf_at_spaces) }
+  let(:external_team_member) { team_members(:maja_at_power) }
   let(:default_context) { DefaultContext.new(user, team) }
 
   describe "#destroy?" do
     it "works" do
-      member = team_members(:maja_at_spaces)
-
-      assert TeamMemberPolicy.new(default_context, member).destroy?
+      assert TeamMemberPolicy.new(default_context, normal_member).destroy?
     end
 
     context "team member does not belong to team" do
       it "returns false" do
-        external_team_member = team_members(:maja_at_power)
-
         refute TeamMemberPolicy.new(default_context, external_team_member).destroy?
       end
     end
 
     context "user removes himself from team" do
       it "returns false" do
-        user_team_member = team_members(:lars_at_spaces)
-
         refute TeamMemberPolicy.new(default_context, user_team_member).destroy?
       end
     end
 
     context "team member to delete is primary owner" do
       it "returns false" do
-        primary_owner = team_members(:ulf_at_spaces)
-
         refute TeamMemberPolicy.new(default_context, primary_owner).destroy?
       end
     end
