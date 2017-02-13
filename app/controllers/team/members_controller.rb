3636eba4c0fd011366651e953e373c2123172859
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-13 20:05:05 +0100

    add controller

---
+++ app/controllers/team/members_controller.rb
@@ -0,0 +1,22 @@
+class Team::MembersController < SubdomainBaseController
+  before_action :set_team, :set_team_member
+
+  # DELETE /team/members/:id
+  def destroy
+    authorize @team, :update?
+
+    @team_member.destroy
+
+    redirect_to team_path
+  end
+
+  private
+
+    def set_team_member
+      @team_member = @team.members.find(params[:id])
+    end
+
+    def set_team
+      @team = current_team
+    end
+end
