a3ef062808477aff953a5644cec4d308d00b2405
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-16 15:32:04 +0100

    a

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@ window.$ = window.jQuery = require('jquery');
 window._ = require('lodash');
 
 require('jquery-ujs');
+require('jquery-ui-bundle');
 
 window.Spaces = {
   Editor: require('./editor')
