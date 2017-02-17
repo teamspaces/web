9028e94fb69c1cb8966a2ece4b6740dbedf9f62a
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-17 11:47:55 +0100

    only init Raven in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,14 +3,11 @@ window._ = require('lodash');
 
 require('jquery-ujs');
 
-window.Raven = require('../../../node_modules/raven-js/dist/raven.js');
-
-
 if(process.env.PRODUCTION){
+  window.Raven = require('../../../node_modules/raven-js/dist/raven.js');
   Raven.config(process.env.SENTRY).install();
 }
 
 window.Spaces = {
   Editor: require('./editor')
 };
-
