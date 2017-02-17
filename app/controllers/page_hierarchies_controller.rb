cb54dc4ae54acc4814bb8033344d0f0e0730b716
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-18 00:22:14 +0100

    extract to interactor

--- app/controllers/page_hierarchies_controller.rb
+++ app/controllers/page_hierarchies_controller.rb
@@ -5,31 +5,14 @@ class PageHierarchiesController < SubdomainBaseController
   def update
     authorize @space, :update?
 
-
-    def save_children(parent:,children:)
-      children_sort = 0
-
-      children.each do |child_attributes|
-        child_page = Page.find(child_attributes["id"])
-        child_page.update(parent_id: parent&.id, sort_order: children_sort)
-
-        children_sort += 1
-
-        if child_attributes["children"]
-          save_children(parent: child_page, children: child_attributes["children"])
-        end
-      end
-    end
-
-    save_children(parent: nil, children: pages_order_params)
-
+    UpdatePageHierarchy.call(hierarchy: page_hierarchy_params)
 
     render json: @space.pages.hash_tree, status: :ok
   end
 
   private
 
-    def pages_order_params
+    def page_hierarchy_params
       JSON.parse(params["page_hierarchy"])
     end
 
