3762ee030a47f3283b34c7403ff722819568e91e
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-02-17 09:50:24 +0100

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@ window._ = require('lodash');
 
 require('jquery-ujs');
 
-Raven.config('https://b99292122b504737bdd0f72a5fe3757e@sentry.io/122307').install();
+Raven.config().install();
 
 window.Spaces = {
   Editor: require('./editor')
