1b943b1a31947b57eee7f4835cd0ffe412ba23f7
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-03-07 13:51:48 +0100

    select user for new team

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resource :team, only: [:show, :edit, :update, :destroy]
+    resource :team, only: [:show, :edit, :update, :destroy, :new], path_names: { new: "new/:user_id"}
     namespace :team do
       resource :user, only: [:new]
       resources :members, only: [:destroy]
