753f5b875477dfa3b54d552a5caa81cbaf76a1f1
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-03-27 20:57:51 -0600

    update

---
+++ test/policies/available_users_policy_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+describe AvailableUsersPolicy, :model do
+  let(:available_user) { users(:ulf) }
+  let(:external_user) { users(:lars) }
+  let(:available_users) do
+    available_users = AvailableUsersQuery.new
+    available_users.stubs(:users)
+                   .returns([available_user])
+  end
+  subject { AvailableUsersPolicy }
+
+  describe "#create_team?" do
+    context "available_user" do
+      it "returns true" do
+        refute subject.new(available_users, available_user).create_team?
+      end
+    end
+
+    context "external_user" do
+      it "returns false" do
+        assert subject.new(available_users, external_user).create_team?
+      end
+    end
+  end
+end
