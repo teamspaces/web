3bfde7efa89900060c36b0f619cebe6833d29d3b
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-03-27 21:16:03 -0600

    add test

--- test/policies/available_users_policy_test.rb
+++ test/policies/available_users_policy_test.rb
@@ -7,19 +7,20 @@ describe AvailableUsersPolicy, :model do
     available_users = AvailableUsersQuery.new(browser_id: "default")
     available_users.stubs(:users)
                    .returns([available_user])
+    available_users
   end
   subject { AvailableUsersPolicy }
 
   describe "#create_team?" do
     context "available_user" do
       it "returns true" do
-        refute subject.new(available_users, available_user).create_team?
+        assert subject.new(available_users, available_user).create_team?
       end
     end
 
     context "external_user" do
       it "returns false" do
-        assert subject.new(available_users, external_user).create_team?
+        refute subject.new(available_users, external_user).create_team?
       end
     end
   end
