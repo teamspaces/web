852c43346c30b7d2e7c1cb81a789faa8d2b4f539
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-03-27 21:07:02 -0600

    update test

--- test/controllers/teams_controller_test.rb
+++ test/controllers/teams_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 describe TeamsController do
   let(:user) { users(:lars) }
+  let(:available_user) { users(:ulf) }
   let(:team) { teams(:spaces) }
   let(:auth_token) { "secret_token" }
   let(:external_user) { users(:with_two_spaces) }
@@ -29,11 +30,11 @@ describe TeamsController do
   describe "#new" do
     context "user for team is authorized" do
       it "works" do
-        stub_available_users_policy_for(user: user,
+        stub_available_users_policy_for(user: available_user,
                                         method: :create_team?,
                                         return_value: true)
 
-        get new_team_url(user, subdomain: team.subdomain)
+        get new_team_url(available_user, subdomain: team.subdomain)
         assert_response :success
       end
     end
@@ -57,7 +58,7 @@ describe TeamsController do
 
     describe "user for team is authorized" do
       before(:each) do
-        stub_available_users_policy_for(user: user,
+        stub_available_users_policy_for(user: available_user,
                                         method: :create_team?,
                                         return_value: true)
       end
@@ -65,9 +66,9 @@ describe TeamsController do
       context "valid params" do
         it "works" do
           assert_difference -> { Team.count }, 1 do
-            post team_url(user_id: user.id, subdomain: team.subdomain), params: valid_params
+            post team_url(user_id: available_user.id, subdomain: team.subdomain), params: valid_params
 
-            assert_redirected_to @controller.sign_in_url_for(user: user, created_team_to_redirect_to: Team.last)
+            assert_redirected_to @controller.sign_in_url_for(user: available_user, created_team_to_redirect_to: Team.last)
           end
         end
       end
@@ -75,7 +76,7 @@ describe TeamsController do
       context "invalid params" do
         it "works" do
           assert_difference -> { Team.count }, 0 do
-            post team_url(user_id: user.id, subdomain: team.subdomain), params: invalid_params
+            post team_url(user_id: available_user.id, subdomain: team.subdomain), params: invalid_params
 
             assert_response :success
           end
