5974d14038d487d43ce010221e75bb4b793a9fd5
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-03-29 12:35:51 -0600

    improve tests

--- test/queries/destroy_user_sessions_query_test.rb
+++ test/queries/destroy_user_sessions_query_test.rb
@@ -6,6 +6,7 @@ describe DestroyUserSessionsQuery, :model do
   let(:user) { users(:with_several_teams) }
   let(:spaces_team) { teams(:spaces) }
   let(:maja_spaces_session) { authie_sessions(:maja_at_spaces) }
+  let(:sven_spaces_session) { authie_sessions(:sven_at_spaces) }
   let(:hassan_without_spaces_session) { authie_sessions(:hassan_at_without_space) }
   let(:maja_power_session) { authie_sessions(:maja_at_power) }
   let(:maja_inactive_spaces_session) { authie_sessions(:maja_inactive_at_spaces) }
@@ -16,23 +17,24 @@ describe DestroyUserSessionsQuery, :model do
   describe "#for_team!" do
     context "single user" do
       it "invalidates all team sessions for user" do
-        sessions = subject.new(user: user).send(:active_user_sessions_for_team, spaces_team)
+        subject.new(user: user).for_team!(spaces_team)
 
-        assert_includes sessions, maja_spaces_session
-        assert_not_includes sessions, hassan_without_spaces_session
-        assert_not_includes sessions, maja_power_session
-        assert_not_includes sessions, maja_inactive_spaces_session
+        refute maja_spaces_session.reload.active
+        assert maja_power_session.reload.active
+        assert sven_spaces_session.reload.active
+        refute maja_inactive_spaces_session.reload.active
+        assert hassan_without_spaces_session.reload.active
       end
     end
 
     context "all users" do
       it "invalidates all team sessions for users" do
-        sessions = subject.new(user: :all).send(:active_user_sessions_for_team, spaces_team)
+        subject.new(user: :all).for_team!(spaces_team)
 
-        assert_includes sessions, maja_spaces_session
-        assert_not_includes sessions, hassan_without_spaces_session
-        assert_not_includes sessions, maja_power_session
-        assert_not_includes sessions, maja_inactive_spaces_session
+        refute maja_spaces_session.reload.active
+        refute sven_spaces_session.reload.active
+        assert hassan_without_spaces_session.reload.active
+        assert maja_power_session.reload.active
       end
     end
   end
@@ -40,25 +42,24 @@ describe DestroyUserSessionsQuery, :model do
   describe "#for_browser!" do
     context "single user" do
       it "invalidates all browser sessions for user" do
-        sessions = subject.new(user: user).send(:active_user_sessions_for_browser, maja_spaces_session.browser_id)
+        subject.new(user: user).for_browser!(maja_spaces_session.browser_id)
 
-        assert_includes sessions, maja_spaces_session
-        assert_includes sessions, maja_power_session
-        assert_not_includes sessions, hassan_without_spaces_session
-        assert_not_includes sessions, maja_inactive_spaces_session
-        assert_not_includes sessions, maja_mobile_browser_session
+        refute maja_spaces_session.reload.active
+        refute maja_power_session.reload.active
+        assert hassan_without_spaces_session.reload.active
+        refute maja_inactive_spaces_session.reload.active
+        assert maja_mobile_browser_session.reload.active
       end
     end
 
     context "all users" do
       it "invalidates all browser sessions for all users" do
-        sessions = subject.new(user: :all).send(:active_user_sessions_for_browser, maja_spaces_session.browser_id)
+        subject.new(user: :all).for_browser!(maja_spaces_session.browser_id)
 
-        assert_includes sessions, maja_spaces_session
-        assert_includes sessions, maja_power_session
-        assert_includes sessions, hassan_without_spaces_session
-        assert_not_includes sessions, maja_inactive_spaces_session
-        assert_not_includes sessions, maja_mobile_browser_session
+        refute maja_spaces_session.reload.active
+        refute maja_power_session.reload.active
+        refute hassan_without_spaces_session.reload.active
+        assert maja_mobile_browser_session.reload.active
       end
     end
   end
