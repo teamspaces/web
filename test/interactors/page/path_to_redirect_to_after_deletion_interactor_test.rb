01a8ee6f73779062591cdc193f07138d357b5bd6
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-04-01 11:42:04 -0600

    update test

--- test/interactors/page/path_to_redirect_to_after_deletion_interactor_test.rb
+++ test/interactors/page/path_to_redirect_to_after_deletion_interactor_test.rb
@@ -4,28 +4,30 @@ describe Page::PathToRedirectToAfterDeletionInteractor, :controller do
   subject { Page::PathToRedirectToAfterDeletionInteractor }
 
   let(:controller) { get root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"]); @controller }
-  let(:marketing_page) { pages(:marketing) }
-  let(:onboarding_page) { pages(:onboarding) }
+  let(:page_to_delete) { pages(:onboarding) }
+  let(:page_to_redirect) { pages(:marketing) }
+  let(:space_page) { pages(:lowest_sort_order) }
   let(:only_page_in_space) { pages(:private_slack_user_milad) }
 
   describe "#call" do
     context "page_to_redirect_to provided" do
-      context "page_to_redirect_to is not page to delete" do
+      context "page_to_redirect_to is not same as page_to_delete" do
         it "redirects to edit page_to_redirect_to" do
-          assert_equal controller.edit_page_path(marketing_page), subject.call(controller: controller,
-                                                                               page_to_delete: onboarding_page,
-                                                                               page_to_redirect: marketing_page).path
+          redirect_path = subject.call(page_to_delete: page_to_delete,
+                                       page_to_redirect: page_to_redirect,
+                                       controller: controller).path
 
+          assert_equal controller.edit_page_path(page_to_redirect), redirect_path
         end
       end
 
-      context "page_to_redirect_to is page to delete" do
-        it "redirects to edit another page" do
+      context "page_to_redirect_to is same as page_to_delete" do
+        it "does not redirect to page_to_redirect_to" do
           redirect_path = subject.call(controller: controller,
-                                       page_to_delete: marketing_page,
-                                       page_to_redirect: marketing_page).path
+                                       page_to_delete: page_to_redirect,
+                                       page_to_redirect: page_to_redirect).path
 
-          assert_not_equal controller.edit_page_path(marketing_page), redirect_path
+          assert_not_equal controller.edit_page_path(page_to_redirect), redirect_path
         end
       end
     end
@@ -33,17 +35,24 @@ describe Page::PathToRedirectToAfterDeletionInteractor, :controller do
     context "no page_to_redirect_to provided" do
       context "several other pages exist in space" do
         it "redirects to next_page" do
-          subject.any_instance.expects(:next_page).returns(onboarding_page)
+          subject.any_instance
+                 .expects(:next_page)
+                 .at_least_once
+                 .returns(space_page)
 
-          assert_equal controller.edit_page_path(onboarding_page), subject.call(controller: controller,
-                                                                                page_to_delete: marketing_page).path
+          redirect_path = subject.call(page_to_delete: page_to_redirect,
+                                       controller: controller).path
+
+          assert_equal controller.edit_page_path(space_page), redirect_path
         end
       end
 
       context "page_to_delete is only page in space" do
         it "redirects to space_pages_path" do
-          assert_equal controller.space_pages_path(only_page_in_space.space), subject.call(controller: controller,
-                                                                                           page_to_delete: only_page_in_space).path
+          redirect_path = subject.call(page_to_delete: only_page_in_space,
+                                       controller: controller).path
+
+          assert_equal controller.space_pages_path(only_page_in_space.space), redirect_path
         end
       end
     end
