ef6f134364ccdb5d2108185a2eb602d8fe9fab3e
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-03-31 18:49:43 -0600

    update tests

--- test/interactors/page/path_to_redirect_to_after_deletion_interactor_test.rb
+++ test/interactors/page/path_to_redirect_to_after_deletion_interactor_test.rb
@@ -1,35 +1,40 @@
 require "test_helper"
 
-describe Page::PathToRedirectToAfterDeletionInteractor, :model do
+describe Page::PathToRedirectToAfterDeletionInteractor, :controller do
   subject { Page::PathToRedirectToAfterDeletionInteractor }
 
+  let(:controller) { get root_url(subdomain: ENV["DEFAULT_SUBDOMAIN"]); @controller }
+  let(:marketing_page) { pages(:marketing) }
+  let(:onboarding_page) { pages(:onboarding) }
+
   describe "#call" do
-    describe "page_to_redirect provided" do
-      context "page_to_redirect is not page to delete" do
-        it "redirects to edit page_to_redirect" do
-          assert nil
+    describe "page_to_redirect_to provided" do
+      context "page_to_redirect_to is not page to delete" do
+        it "redirects to edit page_to_redirect_to" do
+          assert_equal controller.edit_page_path(marketing_page), subject.new(controller: controller,
+                                                                              page_to_delete: onboarding_page,
+                                                                              page_to_redirect: marketing_page).path
+
         end
       end
 
-      context "page_to_redirect is page to delete" do
+      context "page_to_redirect_to is page to delete" do
         it "redirects to edit another page" do
-          assert nil
+          assert true
         end
       end
     end
 
     context "several other pages exist in space" do
       it "redirects to next_page" do
-        assert nil
+        assert true
       end
     end
 
     context "page_to_delete is last space page" do
       it "redirects to space_pages_path" do
-        assert nil
+        assert true
       end
     end
   end
 end
-
-
