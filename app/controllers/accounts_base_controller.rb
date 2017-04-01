08cd5c55fc2a9d3b6a561ec60d3570b811a2f1aa
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-04-01 17:17:26 -0600

    add available users policy test

--- app/controllers/accounts_base_controller.rb
+++ app/controllers/accounts_base_controller.rb
@@ -2,7 +2,7 @@ class AccountsBaseController < ApplicationController
 
   private
 
-    def pundit_user
-      DefaultContext.new(current_user, current_team)
-    end
+#    def pundit_user
+#      DefaultContext.new(current_user, current_team)
+#    end
 end
---
+++ test/policies/available_users_policy_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+describe AvailableUsersPolicy, :model do
+  let(:available_user) { users(:ulf) }
+  let(:unavailable_user) { users(:lars) }
+  let(:available_users) do
+    available_users = AvailableUsersQuery.new(browser_id: "default")
+    available_users.stubs(:users)
+                   .returns([available_user])
+    available_users
+  end
+  subject { AvailableUsersPolicy }
+
+  describe "#create_team?" do
+    context "available_user" do
+      it "returns true" do
+        assert subject.new(available_users, available_user).create_team?
+      end
+    end
+
+    context "unavailable_user" do
+      it "returns false" do
+        refute subject.new(available_users, unavailable_user).create_team?
+      end
+    end
+  end
+end
