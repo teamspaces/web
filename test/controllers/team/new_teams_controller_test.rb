e808e1b5f5c7264207497cb916d1454d10f91838
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-04-01 18:34:52 -0600

    update

--- test/controllers/team/new_teams_controller_test.rb
+++ test/controllers/team/new_teams_controller_test.rb
@@ -2,9 +2,16 @@ require "test_helper"
 
 describe Team::NewTeamsController do
   let(:user) { users(:lars) }
+  let(:available_user) { users(:ulf) }
+  let(:unavailable_user) { users(:sven) }
   before(:each) { sign_in user }
   before(:each) do
-    ApplicationController.any_instance.stubs(:available_users)
+    ApplicationController.any_instance
+                         .stubs(:available_users)
+                         .returns(available_users_mock = mock)
+
+    available_users_mock.stubs(:users)
+                        .returns([user, available_user])
   end
 
   describe "#index" do
@@ -15,14 +22,46 @@ describe Team::NewTeamsController do
   end
 
   describe "#new" do
-    it "works" do
+    context "with available_user" do
+      it "works" do
+        get new_team_new_team_url(available_user, subdomain: ENV["ACCOUNTS_SUBDOMAIN"])
+        assert_response :success
+      end
+    end
 
+    context "with unavailable_user" do
+      it "works" do
+        get new_team_new_team_url(unavailable_user, subdomain: ENV["ACCOUNTS_SUBDOMAIN"])
+        assert_response :success
+      end
     end
   end
 
-  describe "#create" do
-    it "works" do
+  describe "create" do
+    describe "valid team params" do
+      def post_valid_params
+        valid_params = { user_id: available_user.id, team: { name: "bain ltd", subdomain: "baincompany" } }
 
+        post team_new_teams_url(subdomain: ENV["ACCOUNTS_SUBDOMAIN"]), params: valid_params
+      end
+
+      it "creates a team" do
+        assert_difference -> { Team.count }, 1 do
+          post_valid_params
+        end
+      end
+
+      it "redirects to sign_in_url for user and created team" do
+        post_valid_params
+
+        assert_redirected_to @controller.sign_in_url_for(user: available_user, created_team_to_redirect_to: Team.last)
+      end
+    end
+
+    context "invalid team params" do
+      it "works" do
+
+      end
     end
   end
 end
