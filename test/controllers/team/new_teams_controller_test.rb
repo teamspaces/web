cd15a734a58a59c4ec3d44d897f1fc4eb5525302
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-04-02 12:14:33 -0600

    test new team controller

--- test/controllers/team/new_teams_controller_test.rb
+++ test/controllers/team/new_teams_controller_test.rb
@@ -30,37 +30,51 @@ describe Team::NewTeamsController do
     end
 
     context "with unavailable_user" do
-      it "works" do
-        get new_team_new_team_url(unavailable_user, subdomain: ENV["ACCOUNTS_SUBDOMAIN"])
-        assert_response :success
+      it "raises authorization error" do
+        assert_raise Pundit::NotAuthorizedError do
+          get new_team_new_team_url(unavailable_user, subdomain: ENV["ACCOUNTS_SUBDOMAIN"])
+        end
       end
     end
   end
 
   describe "create" do
-    describe "valid team params" do
-      def post_valid_params
-        valid_params = { user_id: available_user.id, team: { name: "bain ltd", subdomain: "baincompany" } }
+    let(:valid_params) { { user_id: available_user.id, team: { name: "bain ltd", subdomain: "baincompany" } } }
+    let(:unavailable_user_params) { { user_id: unavailable_user.id, team: { name: "bain ltd", subdomain: "baincompany" } } }
+    let(:invalid_params) { { user_id: available_user.id, team: { name: "bain ltd" } } }
 
-        post team_new_teams_url(subdomain: ENV["ACCOUNTS_SUBDOMAIN"]), params: valid_params
-      end
+    def post_params params
+      post team_new_teams_url(subdomain: ENV["ACCOUNTS_SUBDOMAIN"]), params: valid_params
+    end
 
+    context "with valid team params" do
       it "creates a team" do
         assert_difference -> { Team.count }, 1 do
-          post_valid_params
+          post_params valid_params
         end
       end
 
       it "redirects to sign_in_url for user and created team" do
-        post_valid_params
+        post_params valid_params
 
         assert_redirected_to @controller.sign_in_url_for(user: available_user, created_team_to_redirect_to: Team.last)
       end
     end
 
-    context "invalid team params" do
+    context "with invalid team params" do
       it "works" do
+        assert_difference -> { Team.count }, 0 do
+          post_params invalid_params
+          assert_response :success
+        end
+      end
+    end
 
+    context "with unavailable user" do
+      it "raises authorization error" do
+        assert_raise Pundit::NotAuthorizedError do
+          post_params unavailable_user_params
+        end
       end
     end
   end
