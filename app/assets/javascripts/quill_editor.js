2cd815cdfef21bb9d2bedd886f098c378ae315c8
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-04-06 12:06:02 -0600

    extract helpers

--- app/assets/javascripts/quill_editor.js
+++ app/assets/javascripts/quill_editor.js
@@ -1,5 +1,5 @@
 const Quill = require("quill");
-import isWhitespace from 'is-whitespace';
+import { clipboardURLMatcherFunc, liveAutolinkUrlsFunc } from './quill_editor_helpers'
 
 /* https://quilljs.com/docs/configuration/ */
 const QuillOptions = { theme: "snow",
@@ -15,50 +15,6 @@ const QuillOptions = { theme: "snow",
                 ]
             }
         };
-
-const clipboardURLMatcherFunc = function(node, delta){
-        var regex = /https?:\/\/[^\s]+/g;
-        if(typeof(node.data) !== 'string') return;
-        var matches = node.data.match(regex);
-
-        if(matches && matches.length > 0) {
-            var ops = [];
-            var str = node.data;
-            matches.forEach(function(match) {
-                var split = str.split(match);
-                var beforeLink = split.shift();
-                ops.push({ insert: beforeLink });
-                ops.push({ insert: match, attributes: { link: match } });
-                str = split.join(match);
-            });
-            ops.push({ insert: str });
-            delta.ops = ops;
-        }
-
-        return delta;
-    };
-
-const liveAutolinkUrlsFunc = function(delta, editor){
-  var regex = /https?:\/\/[^\s]+$/;
-  if(delta.ops.length === 2 && delta.ops[0].retain && isWhitespace(delta.ops[1].insert)) {
-    var endRetain = delta.ops[0].retain;
-    var text = editor.getText().substr(0, endRetain);
-    var match = text.match(regex);
-    if(match !== null) {
-      var url = match[0];
-
-      var ops = [];
-      if(endRetain > url.length) {
-        ops.push({ retain: endRetain - url.length });
-      }
-
-      ops = ops.concat([{ delete: url.length },
-                        { insert: url, attributes: { link: url } } ]);
-
-      editor.updateContents({ ops: ops });
-    }
-  }
-}
 
 class QuillEditor {
 
