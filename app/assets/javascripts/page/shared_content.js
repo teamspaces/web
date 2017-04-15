202ebea63f73fd5f4cec2b9e4e4c007816fe6211
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2017-04-15 16:10:28 +0200

    update/ remove duplications

--- app/assets/javascripts/page/shared_content.js
+++ app/assets/javascripts/page/shared_content.js
@@ -8,15 +8,16 @@ class PageSharedContent extends EventEmitter {
     // emits page_update
     // emits expired_token
 
-    constructor({ collab_url, collection, document_id, url, csrf_token }){
-     // this.collab_url = collab_url;
-     // this.collection = collection;
-     // this.document_id = document_id;
+    constructor(options){
       super();
 
-      this.url = url;
-      this.csrf_token = csrf_token;
+      this.url = options.url;
+      this.csrf_token = options.csrf_token;
 
+      this.init_connection(options);
+    };
+
+    init_connection({ collab_url, collection, document_id, url, csrf_token }){
       this.webSocket = new WebSocket(collab_url);
       this.shareDBConnection = new ShareDB.Connection(this.webSocket);
       this.page = this.shareDBConnection.get(collection, document_id);
@@ -62,39 +63,7 @@ class PageSharedContent extends EventEmitter {
         credentials: 'same-origin'
       }).then(r => r.json())
         .then(response => {
-        console.log(response.editor_settings);
-
-        let opt = JSON.parse(response.editor_settings);
-
-        this.webSocket = new WebSocket(opt.collab_url);
-        this.shareDBConnection = new ShareDB.Connection(this.webSocket);
-        this.page = this.shareDBConnection.get(opt.collection, opt.document_id);
-
-        this.page.subscribe((error) => {
-          //if (error) {
-          //  console.log("error");
-          //  console.log(error);
-         // }
-
-          this.emit('page_subscribe', this.page.data);
-        });
-
-        this.page.on('error', (err) => {
-        if(err.code == 405){
-
-
-          this.emit('expired_token');
-          this.reconnect();
-        };
-
-        console.log("VICOT");
-        console.log(err.code);
-        console.log(err);
-      });
-
-      this.page.on("op", (op, source) => {
-        this.emit('page_update', op, source);
-      });
+          this.init_connection(JSON.parse(response.editor_settings));
       });
     };
 
