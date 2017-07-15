516138a69a3c314a0ffa22224eea5b29e2a793fc
Karl Ollars <karlmagnusollars@gmail.com>
2017-07-15 14:10:41 +0200

    wip

--- app/assets/javascripts/page/title.js
+++ app/assets/javascripts/page/title.js
@@ -9,11 +9,6 @@ class PageTitle {
     this.page = page;
 
     this.addEventListeners();
-  };
-
-  clean() {
-    // TODO Clean input and keep caret position
-    // this.input.html( this.input.text() );
   }
 
   title() {
@@ -24,7 +19,7 @@ class PageTitle {
     this.page.update({title: this.title()})
         .then(response => this.statusMessage.update("All changes have been saved."))
         .catch(error => Raven.captureException(error));
-  };
+  }
 
   addEventListeners() {
     this.input.on('input', (e) => {
@@ -39,7 +34,33 @@ class PageTitle {
           this.save();
         }, SaveAfterMilliseconds);
     });
-  };
-};
+  }
+
+  clean() {
+    let titleHtml = this.input.html().replace(/&nbsp;/g, ' ')
+    let titleText = this.input.text()
+
+    // Clean content if it's not plain text
+    if(titleHtml.replace(/\s/g, '') != titleText.replace(/\s/g, '')) {
+      this.input.html( titleText )
+
+      this.moveCursorToEnd(this.input.get(0))
+    }
+  }
+
+  moveCursorToEnd(contentEditableElement) {
+    var range,selection;
+
+    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+    {
+        range = document.createRange();//Create a range (a range is a like the selection but invisible)
+        range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        selection = window.getSelection();//get the selection object (allows you to change selection)
+        selection.removeAllRanges();//remove any selections already made
+        selection.addRange(range);//make the range you have just created the visible selection
+    }
+  }
+}
 
 export default PageTitle;
