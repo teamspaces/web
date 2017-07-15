14042f5d0af0b3386a1a2ec98db4a28fd937df20
emmanuelmillionaer <stadleremmanuel1@gmail.com>
2017-07-15 15:18:26 +0200

    use webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,27 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('Hello World from Webpacker');
+
+
+import Editor from '../javascripts/editor'
+import Page from '../javascripts/page'
+import PageStatusMessage from '../javascripts/page/status_message'
+import PageTitle from '../javascripts/page/title'
+import PageTree from '../javascripts/page_tree'
+import PageHierarchy from '../javascripts/page_hierarchy'
+import Dropdowns from '../javascripts/dropdowns'
+
+window.Spaces = {
+  Editor: Editor,
+  Page: Page,
+  PageStatusMessage: PageStatusMessage,
+  PageTitle: PageTitle,
+  PageTree: PageTree,
+  PageHierarchy: PageHierarchy
+};
+
+const dropdowns = new Dropdowns()
 
 
 import '../stylesheets/application.scss';
