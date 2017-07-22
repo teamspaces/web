cbb8257eb1f4f896298db11d47daa7c0dc7abf1f
Martin Samami <martin@digitalkookie.io>
2017-07-22 12:01:33 +0200

    Fix enable AWS ES

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -2,7 +2,7 @@
 Searchkick.timeout = 3
 
 # AWS ES Support
-if ENV["AWS_ELASTICSEARCH_URL"].present?
+if ENV["ENABLE_AWS_ELASTICSEARCH"] == "true"
   Searchkick.aws_credentials = {
     access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
