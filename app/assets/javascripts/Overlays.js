ef6c44bbec7a1fe8b3324c24334b8a0004b13102
Karl Ollars <karlmagnusollars@gmail.com>
2017-07-23 22:44:20 +0200

    wip

---
+++ app/assets/javascripts/Overlays.js
@@ -0,0 +1,68 @@
+import $ from 'jquery'
+
+class Overlays {
+  constructor () {
+    // Add event listeners when the dom is ready
+    $(this.addListeners.bind(this))
+  }
+
+  addListeners () {
+    // Adds event listeners with a .overlays namespace
+    $('.overlay__toggle').on('click.overlays', this.onToggleClick.bind(this))
+    $(document).on('click.overlays', this.onDocumentClick.bind(this))
+    $(document).on('keyup.overlays', this.onDocumentKeyUp.bind(this))
+  }
+
+  removeListeners () {
+    // Removes event listeners with the .dropdowns namespace
+    $('.overlay__toggle').off('.overlays')
+    $(document).off('.overlays')
+  }
+
+  onToggleClick (e) {
+    e.preventDefault()
+
+    const $toggle = $(e.currentTarget)
+    const $overlay = $( $toggle.attr('href') )
+
+    this.hideOtherOverlays($overlay)
+    this.toggleOverlay($overlay)
+  }
+
+  onDocumentClick (e) {
+    // Hide all overlays if the click was not within a overlay
+    if(!$(e.target).closest('.overlay, .overlay__toggle').length)
+      this.hideAllOverlays()
+  }
+
+  onDocumentKeyUp (e) {
+    // Pressing escape hides all overlays
+    if(e.keyCode === 27)
+      this.hideAllOverlays()
+  }
+
+  toggleOverlay(overlay) {
+    overlay.toggleClass('overlay--visible')
+  }
+
+  hideOtherOverlays (overlay) { console.log('hide other')
+    const visible = $('.overlay--visible')
+
+    // Hide any visible overlays (but not if it's the new one or the new overlay is nested within it)
+    visible.each(function(i, el) {
+      if(!$(el).has(overlay).length && !$(el).is(overlay))
+        $(el).removeClass('overlay--visible')
+    })
+  }
+
+  hideAllOverlays () { console.log('hide all')
+    $('.overlay').removeClass('overlay--visible')
+  }
+
+  destroy () {
+    this.hideAllOverlays()
+    this.removeListeners()
+  }
+}
+
+export default Overlays
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,11 @@
+import './helpers'
 import Editor from './editor'
 import Page from './page'
 import PageStatusMessage from './page/status_message'
 import PageTitle from './page/title'
 import PageTree from './PageTree'
 import PageHierarchy from './PageHierarchy'
-import Dropdowns from './dropdowns'
 import SpaceSidebar from './SpaceSidebar'
-import tippy from 'tippy.js/dist/tippy'
 
 window.Spaces = {
   Editor: Editor,
@@ -17,21 +16,3 @@ window.Spaces = {
   PageHierarchy: PageHierarchy,
   SpaceSidebar: SpaceSidebar
 }
-
-const dropdowns = new Dropdowns()
-
-// Tooltips
-$(function() {
-  tippy('.tippy', {
-    position: 'top',
-    arrow: true,
-    arrowSize: 'small',
-    animation: 'shift',
-    delay: [100, 0],
-    size: 'regular',
-    theme: 'spaces',
-    sticky: true,
-    animateFill: false,
-    zIndex: 99
-  })
-})
