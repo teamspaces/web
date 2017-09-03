56716277632ec1fef1849796c054b22319ca35f5
emmanuelmillionaer <stadleremmanuel1@gmail.com>
2017-09-03 11:34:15 +0200

    add babel-loader options, plugins

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -1,5 +1,9 @@
 module.exports = {
   test: /\.js(\.erb)?$/,
   exclude: /node_modules/,
-  loader: 'babel-loader'
+  loader: 'babel-loader',
+  query: {
+    plugins: ['transform-runtime'],
+    presets: ['es2015']
+  }
 }
